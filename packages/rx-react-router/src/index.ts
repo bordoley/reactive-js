b6ab191c0505605f15ff75c6fd8ad3d5fa1fc9e5
David Bordoley <bordoley@gmail.com>
2019-11-11 23:47:49 -0500

    add rx router

---
+++ packages/rx-react-router/src/index.ts
@@ -0,0 +1,173 @@
+import {
+  StateUpdater,
+  ObservableStateResourceLike,
+} from "@rx-min/rx-observable-state";
+
+import { useResource, useObservable } from "@rx-min/rx-react";
+
+import { createElement, useCallback, useMemo, Children } from "react";
+import { Observable } from "@rx-min/rx-core";
+import { scan } from "@rx-min/rx-operators";
+import { string } from "prop-types";
+import { parse } from "path";
+
+// React Native doesn't use a standard URI library so define
+// a minimal type that can be passed around
+export interface RelativeURI {
+  readonly path: string;
+  readonly query: string;
+  readonly fragment: string;
+}
+
+const emptyRelativeURI: RelativeURI = {
+  path: "",
+  query: "",
+  fragment: "",
+};
+
+export interface RoutableComponentProps {
+  readonly referer: RelativeURI | undefined;
+  readonly uri: RelativeURI;
+  readonly uriUpdater: (updater: StateUpdater<RelativeURI>) => void;
+}
+
+export interface RoutableStateComponentProps<TState> {
+  readonly uri: RelativeURI;
+  readonly referer: RelativeURI | undefined;
+  readonly goTo: (uri: RelativeURI) => void;
+  readonly state: TState;
+  readonly dispatch: (updater: StateUpdater<TState>) => void;
+}
+
+const createRoutableStateComponent = <TState>(
+  component: React.ComponentType<RoutableStateComponentProps<TState>>,
+  parseState: (fragment: string) => TState,
+  serialize: (state: TState) => string,
+  stateIsQuery = false,
+): React.ComponentType<RoutableComponentProps> => {
+  const createURIStateUpdater = (stateUpdater: StateUpdater<TState>) => (
+    oldURI: RelativeURI,
+  ) => {
+    const oldSerialized = stateIsQuery ? oldURI.query : oldURI.fragment;
+
+    const oldState = parseState(oldSerialized);
+    const newState = stateUpdater(oldState);
+
+    if (oldState === newState) {
+      return oldURI;
+    } else if (stateIsQuery) {
+      const query = serialize(newState);
+      return {
+        ...oldURI,
+        query,
+      };
+    } else {
+      const fragment = serialize(newState);
+      return {
+        ...oldURI,
+        fragment,
+      };
+    }
+  };
+
+  const RoutableStateComponent = ({
+    referer,
+    uri,
+    uriUpdater,
+  }: RoutableComponentProps) => {
+    const state = useMemo(() => {
+      const serialized = stateIsQuery ? uri.query : uri.fragment;
+      return parseState(serialized);
+    }, [uri]);
+
+    const goTo: (uri: RelativeURI) => void = useCallback(
+      uri => uriUpdater(_ => uri),
+      [uri, uriUpdater],
+    );
+
+    const dispatch: (updater: StateUpdater<TState>) => void = useCallback(
+      stateUpdater => {
+        uriUpdater(createURIStateUpdater(stateUpdater));
+      },
+      [uriUpdater],
+    );
+
+    return createElement(component, {
+      uri,
+      referer,
+      goTo,
+      state,
+      dispatch,
+    });
+  };
+
+  return RoutableStateComponent;
+};
+
+export const RoutableStateComponent = {
+  create: createRoutableStateComponent,
+};
+
+type RouteMap = {
+  [key: string]: React.ComponentType<RoutableComponentProps>;
+};
+
+const routesReducer = (
+  acc: RouteMap,
+  [path, component]: [string, React.ComponentType<RoutableComponentProps>],
+): RouteMap => {
+  acc[path] = component;
+  return acc;
+};
+
+const pairify = (
+  [_, oldState]: [RelativeURI | undefined, RelativeURI],
+  next: RelativeURI,
+): [RelativeURI | undefined, RelativeURI] =>
+  oldState === emptyRelativeURI ? [undefined, next] : [oldState, next];
+
+const createRouter = <TContext>(
+  locationResourceFactory: () => ObservableStateResourceLike<RelativeURI>,
+  notFoundComponent: React.ComponentType<RoutableComponentProps>,
+  routes: readonly [string, React.ComponentType<RoutableComponentProps>][],
+  context: React.Context<TContext> | void,
+): React.ComponentType<TContext> => {
+  const routeMap = routes.reduce(routesReducer, {});
+
+  const RxReactRouter = (props: TContext) => {
+    const locationResource = useResource(locationResourceFactory, []);
+
+    const uriUpdater = useCallback(
+      (update: StateUpdater<RelativeURI>) => locationResource.dispatch(update),
+      [locationResource],
+    );
+
+    const locationHistoryObservable = useMemo(
+      () =>
+        Observable.lift(
+          locationResource,
+          scan(pairify, [undefined, emptyRelativeURI]),
+        ),
+      [locationResource],
+    );
+
+    const route = useObservable(locationHistoryObservable);
+
+    let child = null;
+    if (route !== undefined) {
+      const [referer, uri] = route;
+      const component = routeMap[uri.path] || notFoundComponent;
+      child = createElement(component, { referer, uri, uriUpdater });
+    }
+
+    return context !== undefined
+      ? createElement(context.Provider, { value: props, children: child })
+      : child;
+  };
+
+  return RxReactRouter;
+};
+
+export const Router = {
+  create: createRouter,
+};
--- packages/rx-react/src/index.ts
+++ packages/rx-react/src/index.ts
@@ -5,10 +5,50 @@ import {
   Notifications,
   Observable,
   ObservableLike,
+  ObservableResourceLike,
   SchedulerLike,
 } from "@rx-min/rx-core";
 
 import { normalPriority } from "@rx-min/rx-scheduler";
+import { DisposableLike } from "@rx-min/rx-disposables";
+
+export const useResource = <T extends DisposableLike>(
+  factory: () => T,
+  deps: readonly any[] | undefined,
+): T => {
+  const resource = useMemo(factory, deps);
+  useEffect(
+    () => () => {
+      resource.dispose();
+    },
+    [resource],
+  );
+
+  return resource;
+};
+
+const makeObservable = <T>(
+  observable: ObservableLike<T>,
+  updateState: React.Dispatch<React.SetStateAction<T | undefined>>,
+  updateError: React.Dispatch<React.SetStateAction<Error | undefined>>,
+) =>
+  Observable.lift(
+    observable,
+    observe({
+      notify: (event: Notification, data: T | Error | undefined) => {
+        switch (event) {
+          case Notifications.next:
+            updateState(_ => data as T);
+            break;
+          case Notifications.complete:
+            if (data != undefined) {
+              updateError(_ => data as Error | undefined);
+            }
+            break;
+        }
+      },
+    }),
+  );
 
 export const useObservable = <T>(
   observable: ObservableLike<T>,
@@ -17,36 +57,13 @@ export const useObservable = <T>(
   const [state, updateState] = useState<T | undefined>(undefined);
   const [error, updateError] = useState<Error | undefined>(undefined);
 
-  const subscription = useMemo(
+  useResource(
     () =>
       Observable.connect(
-        Observable.lift(
-          observable,
-          observe({
-            notify: (event: Notification, data: T | Error | undefined) => {
-              switch (event) {
-                case Notifications.next:
-                  updateState(_ => data as T);
-                  break;
-                case Notifications.complete:
-                  if (data != undefined) {
-                    updateError(_ => data as Error | undefined);
-                  }
-                  break;
-              }
-            },
-          }),
-        ),
+        makeObservable(observable, updateState, updateError),
         scheduler,
       ),
-    [observable, updateState, updateError],
-  );
-
-  useEffect(
-    () => () => {
-      subscription.dispose();
-    },
-    [subscription],
+    [observable, updateState, updateError, scheduler],
   );
 
   if (error !== undefined) {
@@ -54,4 +71,13 @@ export const useObservable = <T>(
   }
 
   return state;
+};
+
+export const useObservableResource = <T>(
+  factory: () => ObservableResourceLike<T>,
+  deps: readonly any[] | undefined,
+  scheduler: SchedulerLike = normalPriority,
+): T | undefined => {
+  const resource = useResource(factory, deps);
+  return useObservable(resource, scheduler);
 };
