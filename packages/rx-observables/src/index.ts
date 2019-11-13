fda239837500dab3311735b8e01056e05b39cbd3
David Bordoley <bordoley@gmail.com>
2019-11-12 20:18:06 -0500

    add fromPromiseFactory observable create function

--- packages/rx-observables/src/index.ts
+++ packages/rx-observables/src/index.ts
@@ -2,6 +2,7 @@ export { concat } from "./concat";
 export { merge } from "./merge";
 export { never } from "./never";
 export { empty, ofArray, ofDelayedValues, ofValue } from "./ofArray";
+export { fromPromiseFactory } from './promise';
 export { repeat, retry } from "./repeat";
 export { throws } from "./throws";
 export { use } from "./use";
---
+++ packages/rx-observables/src/promise.ts
@@ -0,0 +1,23 @@
+import {
+  Observable,
+  ObservableLike,
+  SubscriberLike,
+  Notifications,
+} from "@rx-min/rx-core";
+
+export const fromPromiseFactory = <T>(
+  factory: () => Promise<T>,
+): ObservableLike<T> =>
+  Observable.create(async (subscriber: SubscriberLike<T>) => {
+    try {
+      const result = await factory();
+      if (!subscriber.subscription.isDisposed) {
+        subscriber.notify(Notifications.next, result);
+        subscriber.notify(Notifications.complete, undefined);
+      }
+    } catch (error) {
+      if (!subscriber.subscription.isDisposed) {
+        subscriber.notify(Notifications.complete, error);
+      }
+    }
+  });
