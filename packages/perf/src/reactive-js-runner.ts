959f36a7b8b1e78f7e5e78026021cc1887d7b8e2
David Bordoley <bordoley@gmail.com>
2019-11-16 01:40:11 -0500

    Use a perf test specific scheduler

--- packages/perf/src/reactive-js-runner.ts
+++ packages/perf/src/reactive-js-runner.ts
@@ -1,8 +1,39 @@
-import { connect, Observable, ObservableLike } from "@reactive-js/rx-core";
+import { connect, ObservableLike } from "@reactive-js/rx-core";
+import { Disposable, DisposableLike } from "@reactive-js/disposables";
 import { VirtualTimeScheduler } from "@reactive-js/virtualtime-scheduler";
+import { SchedulerContinuation, SchedulerLike } from "@reactive-js/scheduler";
+
+const shouldYield = () => false;
+
+// This is a level the playing ground scheduler
+// that doesn't honor the true contract of scheduler
+// but does the minimal amount of work to give an
+// apples to apples comparison vs other synchronous implementations
+class PerfTestScheduler implements SchedulerLike {
+  private readonly queue: SchedulerContinuation[] = [];
+
+  get now() {
+    return 0;
+  }
+
+  schedule(
+    continuation: SchedulerContinuation,
+    delay: number = 0,
+  ): DisposableLike {
+    this.queue.push(continuation);
+    return Disposable.empty();
+  }
+
+  run() {
+    let next;
+    while ((next = this.queue.shift()) !== undefined) {
+      next(shouldYield);
+    }
+  }
+}
 
 export const run = <T>(observable: ObservableLike<T>) => {
-  const scheduler = VirtualTimeScheduler.create();
+  const scheduler = new PerfTestScheduler();
   connect(observable, scheduler);
   scheduler.run();
 };
