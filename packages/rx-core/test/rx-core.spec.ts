d6bebfdc1d9ea61af3783ddcf1ec104599f9577e
David Bordoley <bordoley@gmail.com>
2019-11-18 15:08:23 -0500

    more subscriber tests

--- packages/rx-core/test/rx-core.spec.ts
+++ packages/rx-core/test/rx-core.spec.ts
@@ -3,6 +3,7 @@ import {
   observe,
   NotificationKind,
   Observable,
+  ObserverLike,
   SubscriberLike,
 } from "../src/index";
 import { CompositeDisposable, Disposable } from "@reactive-js/disposables";
@@ -22,33 +23,95 @@ test("notify", () => {
   expect(observer.complete).toBeCalledWith(error);
 });
 
-test("observe", () => {
-  const scheduler: SchedulerLike = {
-    now: 0,
-    schedule: (c, d?, p?) => Disposable.disposed,
+describe("observe", () => {
+  const createSubscriber = <T>(): SubscriberLike<T> => {
+    const scheduler: SchedulerLike = {
+      now: 0,
+      schedule: (c, d?, p?) => Disposable.disposed,
+    };
+
+    return {
+      isConnected: true,
+      scheduler,
+      subscription: CompositeDisposable.create(),
+      next: jest.fn(),
+      complete: jest.fn(),
+    };
   };
 
-  const subscriber: SubscriberLike<number> = {
-    isConnected: true,
-    scheduler,
-    subscription: CompositeDisposable.create(),
+  const createObserver = <T>(): ObserverLike<T> => ({
     next: jest.fn(),
     complete: jest.fn(),
-  };
+  });
 
-  const observer = {
-    next: jest.fn(),
-    complete: jest.fn(),
-  };
+  test("next", () => {
+    const subscriber = createSubscriber();
+    const observer = createObserver();
+    const delegatedSubscriber = observe(observer)(subscriber);
 
-  const observeSubscriber = observe(observer)(subscriber);
+    delegatedSubscriber.next("a");
+    expect(observer.next).toBeCalledWith("a");
+    expect(observer.next).toBeCalledWith("a");
+  });
 
-  observeSubscriber.next("a");
-  expect(observer.next).toBeCalledWith("a");
-  expect(subscriber.next).toBeCalledWith("a");
+  test("next when observer throws Error", () => {
+    const subscriber = createSubscriber();
+    const error = new Error();
+    const observer: ObserverLike<any> = {
+      next: _ => {
+        throw error;
+      },
+      complete: _ => {},
+    };
+    const delegatedSubscriber = observe(observer)(subscriber);
 
-  const error = new Error();
-  observeSubscriber.complete(error);
-  expect(observer.complete).toBeCalledWith(error);
-  expect(subscriber.complete).toBeCalledWith(error);
+    delegatedSubscriber.next("a");
+    expect(subscriber.complete).toBeCalledWith(error);
+  });
+
+  test("complete", () => {
+    const subscriber = createSubscriber();
+    const observer = createObserver();
+    const delegatedSubscriber = observe(observer)(subscriber);
+
+    const error = new Error();
+    delegatedSubscriber.complete(error);
+    expect(observer.complete).toBeCalledWith(error);
+    expect(subscriber.complete).toBeCalledWith(error);
+  });
+
+  test("complete when observer throws Error", () => {
+    const subscriber = createSubscriber();
+    const error = new Error();
+    const observer: ObserverLike<any> = {
+      next: _ => { },
+      complete: _ => {
+        throw error;
+      },
+    };
+    const delegatedSubscriber = observe(observer)(subscriber);
+
+    delegatedSubscriber.complete()
+    expect(subscriber.complete).toBeCalledWith(error);
+  });
+
+  test("disposing the subscriber blocks further notifications", () => {
+    const subscriber = createSubscriber();
+    const observer = createObserver();
+    const delegatedSubscriber = observe(observer)(subscriber);
+
+    subscriber.subscription.dispose();
+    delegatedSubscriber.next("a");
+    expect(observer.next).toBeCalledTimes(0);
+  });
+
+  test("completing the subscriber blocks further notifications", () => {
+    const subscriber = createSubscriber();
+    const observer = createObserver();
+    const delegatedSubscriber = observe(observer)(subscriber);
+
+    delegatedSubscriber.complete();
+    delegatedSubscriber.next("a");
+    expect(observer.next).toBeCalledTimes(0);
+  });
 });
