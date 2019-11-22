08f3d22575152c67bccaf8a7d284324914b883ec
David Bordoley <bordoley@gmail.com>
2019-11-22 12:32:13 -0500

    concat tests and refactor

--- packages/rx-observables/src/concat.ts
+++ packages/rx-observables/src/concat.ts
@@ -8,28 +8,6 @@ import {
   SubscriberLike,
 } from "@reactive-js/rx-core";
 
-class ConcatObserver<T> implements ObserverLike<T> {
-  private readonly delegate: ObserverLike<T>;
-  private readonly continuation: () => boolean;
-
-  constructor(delegate: ObserverLike<T>, continuation: () => boolean) {
-    this.delegate = delegate;
-    this.continuation = continuation;
-  }
-
-  next(data: T) {
-    this.delegate.next(data);
-  }
-
-  complete(error?: Error) {
-    if (error !== undefined) {
-      this.delegate.complete(error);
-    } else if (!this.continuation()) {
-      this.delegate.complete();
-    }
-  }
-}
-
 export const concat = <T>(
   fst: ObservableLike<T>,
   snd: ObservableLike<T>,
@@ -40,26 +18,36 @@ export const concat = <T>(
   const subscribe = (subscriber: SubscriberLike<T>) => {
     const queue = [...observables];
 
+    let innerSubscription = Disposable.disposed;
+
     const subscribeNext = () => {
       const head = queue.shift();
+
       if (head !== undefined) {
-        let innerSubscription = Disposable.disposed;
-
-        const continuation = () => {
-          subscriber.remove(innerSubscription);
-          return subscribeNext();
-        };
-        const observer = new ConcatObserver(subscriber, continuation);
-
         innerSubscription = Observable.connect(
           Observable.lift(head, observe(observer)),
           subscriber,
         );
+
         subscriber.add(innerSubscription);
       }
 
       return head !== undefined;
     };
+
+    const next = (v: T) => subscriber.next(v);
+
+    const complete = (error?: Error) => {
+      subscriber.remove(innerSubscription);
+
+      if (error !== undefined) {
+        subscriber.complete(error);
+      } else if(!subscribeNext()) {
+        subscriber.complete();
+      }
+    };
+
+    const observer = { next, complete };
 
     subscribeNext();
   };
