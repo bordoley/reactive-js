5ba1d6e7efd2ab621d36cae59c477e16032f71fc
David Bordoley <bordoley@gmail.com>
2019-11-25 18:10:11 -0500

    add async-iterator pipe functions.

--- packages/ix-async-iterator-resource/src/index.ts
+++ packages/ix-async-iterator-resource/src/index.ts
@@ -3,8 +3,10 @@ import {
   AsyncIteratorLike,
   DelegatingAsyncIterator,
 } from "@reactive-js/ix-async-iterator";
+import { ObservableOperator } from "@reactive-js/rx-observable";
 import {
   lift as observableResourceLift,
+  pipe as observableResourcePipe,
   ObservableResourceLike,
 } from "@reactive-js/rx-observable-resource";
 import { SubscriberLike, SubscriberOperator } from "@reactive-js/rx-subscriber";
@@ -156,3 +158,95 @@ export const map = <TSrcReq, TReq, T>(
 
   return new DelegatingAsyncIteratorResource(delegate, mappedDispatcher);
 };
+
+export function pipe<TReq, T, TA>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+): AsyncIteratorResourceLike<TReq, TA>;
+export function pipe<TReq, T, TA, TB>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+): AsyncIteratorResourceLike<TReq, TB>;
+export function pipe<TReq, T, TA, TB, TC>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+): AsyncIteratorResourceLike<TReq, TC>;
+export function pipe<TReq, T, TA, TB, TC, TD>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+): AsyncIteratorResourceLike<TReq, TD>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+): AsyncIteratorResourceLike<TReq, TE>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+): AsyncIteratorResourceLike<TReq, TF>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+): AsyncIteratorResourceLike<TReq, TG>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG, TH>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+  op8: ObservableOperator<TG, TH>,
+): AsyncIteratorResourceLike<TReq, TH>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG, TH, TI>(
+  src: AsyncIteratorResourceLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+  op8: ObservableOperator<TG, TH>,
+  op9: ObservableOperator<TH, TI>,
+): AsyncIteratorResourceLike<TReq, TI>;
+export function pipe(
+  src: AsyncIteratorResourceLike<any, any>,
+  ...operators: ObservableOperator<any, any>[]
+): AsyncIteratorResourceLike<any, any> {
+  const [delegate, dispatcher] =
+    src instanceof DelegatingAsyncIteratorResource
+      ? [src.disposable, src.dispatcher]
+      : [src, (req: any) => src.dispatch(req)];
+
+  const liftedObservableResource = observableResourcePipe.apply(undefined, [
+    delegate,
+    ...operators,
+  ] as any);
+
+  return new DelegatingAsyncIteratorResource(
+    liftedObservableResource,
+    dispatcher,
+  );
+}
--- packages/ix-async-iterator/src/index.ts
+++ packages/ix-async-iterator/src/index.ts
@@ -1,6 +1,8 @@
 import {
   lift as observableLift,
   ObservableLike,
+  ObservableOperator,
+  pipe as observablePipe,
 } from "@reactive-js/rx-observable";
 import { SubscriberLike, SubscriberOperator } from "@reactive-js/rx-subscriber";
 
@@ -132,3 +134,92 @@ export const map = <TSrcReq, TReq, T>(
 
   return new DelegatingAsyncIterator(delegate, mappedDispatcher);
 };
+
+export function pipe<TReq, T, TA>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+): AsyncIteratorLike<TReq, TA>;
+export function pipe<TReq, T, TA, TB>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+): AsyncIteratorLike<TReq, TB>;
+export function pipe<TReq, T, TA, TB, TC>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+): AsyncIteratorLike<TReq, TC>;
+export function pipe<TReq, T, TA, TB, TC, TD>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+): AsyncIteratorLike<TReq, TD>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+): AsyncIteratorLike<TReq, TE>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+): AsyncIteratorLike<TReq, TF>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+): AsyncIteratorLike<TReq, TG>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG, TH>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+  op8: ObservableOperator<TG, TH>,
+): AsyncIteratorLike<TReq, TH>;
+export function pipe<TReq, T, TA, TB, TC, TD, TE, TF, TG, TH, TI>(
+  src: AsyncIteratorLike<TReq, T>,
+  op1: ObservableOperator<T, TA>,
+  op2: ObservableOperator<TA, TB>,
+  op3: ObservableOperator<TB, TC>,
+  op4: ObservableOperator<TC, TD>,
+  op5: ObservableOperator<TD, TE>,
+  op6: ObservableOperator<TE, TF>,
+  op7: ObservableOperator<TF, TG>,
+  op8: ObservableOperator<TG, TH>,
+  op9: ObservableOperator<TH, TI>,
+): AsyncIteratorLike<TReq, TI>;
+export function pipe(
+  src: AsyncIteratorLike<any, any>,
+  ...operators: ObservableOperator<any, any>[]
+): AsyncIteratorLike<any, any> {
+  const [observable, dispatcher] =
+    src instanceof DelegatingAsyncIterator
+      ? [src.observable, src.dispatcher]
+      : [src, (req: any) => src.dispatch(req)];
+
+  const liftedObservable = observablePipe.apply(undefined, [
+    observable,
+    ...operators,
+  ] as any);
+
+  return new DelegatingAsyncIterator(liftedObservable, dispatcher);
+}
