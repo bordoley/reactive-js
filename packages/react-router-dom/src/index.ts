c7c2341e73173d6c16aedf65bd6467442e3d65e8
David Bordoley <bordoley@gmail.com>
2019-11-26 07:48:53 -0500

    code cleanups and simplifications

--- packages/react-router-dom/src/index.ts
+++ packages/react-router-dom/src/index.ts
@@ -1,154 +1,51 @@
 import { create as createRouter, RouterProps } from "@reactive-js/react-router";
-import { RelativeURI } from "@reactive-js/react-router-relative-uri";
 import {
-  create as stateContainerCreate,
-  StateContainerLike,
-  StateContainerResourceLike,
-  StateUpdater,
-} from "@reactive-js/state-container";
-
-import {
-  asyncIteratorResourceOperatorFrom,
-  mapDispatch,
-  pipe as asyncIteratorResourcePipe,
-} from "@reactive-js/ix-async-iterator-resource";
-import {
-  connect,
-  lift,
-  pipe as observablePipe,
-} from "@reactive-js/rx-observable";
-
-import {
-  distinctUntilChanged,
-  keep,
-  map,
-  merge,
-  onNext,
-} from "@reactive-js/rx-observables";
-
+  equals as relativeURIEquals,
+  RelativeURI,
+} from "@reactive-js/react-router-relative-uri";
+import { create as stateContainerCreate } from "@reactive-js/state-container";
+import { connect, pipe } from "@reactive-js/rx-observable";
+import { keep, merge, onNext } from "@reactive-js/rx-observables";
 import { scheduler } from "@reactive-js/react-scheduler";
-
-import {
-  create as disposableCreate,
-  DisposableLike,
-  DisposableOrTeardown,
-} from "@reactive-js/disposable";
-import { SubscriberLike } from "@reactive-js/rx-subscriber";
-import { SchedulerLike } from "@reactive-js/scheduler";
-
 import { fromEvent } from "@reactive-js/dom";
 
-const getCurrentLocation = () => {
+const getCurrentLocation = (): RelativeURI => {
   const path = window.location.pathname;
   const query = window.location.search;
   const fragment = window.location.hash;
-  return path + query + fragment;
+  return { path, query, fragment };
 };
 
-class DomLocationStateContainerResourceImpl
-  implements StateContainerResourceLike<string> {
-  get isDisposed(): boolean {
-    return this.stateContainer.isDisposed;
-  }
-  private readonly stateContainer: StateContainerResourceLike<string>;
-
-  constructor(scheduler?: SchedulerLike, priority?: number) {
-    this.stateContainer = stateContainerCreate(
+export const create = (priority?: number): React.ComponentType<RouterProps> =>
+  createRouter(() => {
+    const stateContainer = stateContainerCreate(
       getCurrentLocation(),
-      undefined,
+      relativeURIEquals,
       scheduler,
       priority,
     );
 
-    const subscription = connect(
-      merge(
-        observablePipe(
-          fromEvent(window, "popstate", _ => getCurrentLocation(), priority),
-          onNext((state: string) => this.stateContainer.dispatch(_ => state)),
-        ),
-        observablePipe(
-          this.stateContainer,
-          keep(location => location !== getCurrentLocation()),
-          onNext((next: string) =>
-            window.history.pushState(undefined, "", next),
+    stateContainer.add(
+      connect(
+        merge(
+          pipe(
+            fromEvent(window, "popstate", _ => getCurrentLocation(), priority),
+            onNext((state: RelativeURI) => stateContainer.dispatch(_ => state)),
+          ),
+          pipe(
+            stateContainer,
+            keep(
+              location => !relativeURIEquals(location, getCurrentLocation()),
+            ),
+            onNext(({ path, query, fragment }: RelativeURI) => {
+              const uri = path + query + fragment;
+              window.history.pushState(undefined, "", uri);
+            }),
           ),
         ),
+        scheduler,
       ),
-      scheduler,
     );
 
-    this.stateContainer.add(subscription);
-  }
-
-  add(
-    disposable: DisposableOrTeardown,
-    ...disposables: DisposableOrTeardown[]
-  ) {
-    this.stateContainer.add(disposable, ...disposables);
-  }
-
-  dispatch(updater: StateUpdater<string>) {
-    this.stateContainer.dispatch(updater);
-  }
-
-  dispose() {
-    this.stateContainer.dispose();
-  }
-
-  remove(
-    disposable: DisposableOrTeardown,
-    ...disposables: DisposableOrTeardown[]
-  ) {
-    this.stateContainer.remove(disposable, ...disposables);
-  }
-
-  subscribe(subscriber: SubscriberLike<string>) {
-    this.stateContainer.subscribe(subscriber);
-  }
-}
-
-const fakeURLBase = new URL("http://example.com");
-
-const mapper = (v: string): RelativeURI => {
-  const parsedAccURL = new URL(v, fakeURLBase);
-  return {
-    path: parsedAccURL.pathname,
-    query: parsedAccURL.search,
-    fragment: parsedAccURL.hash,
-  };
-};
-
-const reducer = (
-  acc: string,
-  stateUpdater: StateUpdater<RelativeURI>,
-): string => {
-  const parsedAccURL = new URL(acc, fakeURLBase);
-  const accRelativeURI = {
-    path: parsedAccURL.pathname,
-    query: parsedAccURL.search,
-    fragment: parsedAccURL.hash,
-  };
-
-  const { path, query, fragment } = stateUpdater(accRelativeURI);
-  return path + query + fragment;
-};
-
-const requestMapper = (updater: StateUpdater<RelativeURI>) => (acc: string) =>
-  reducer(acc, updater);
-
-const createRelativeURILocation = (priority?: number) =>
-  asyncIteratorResourcePipe(
-    new DomLocationStateContainerResourceImpl(scheduler, priority),
-    asyncIteratorResourceOperatorFrom(map(mapper)),
-    asyncIteratorResourceOperatorFrom(distinctUntilChanged()),
-    asyncIteratorResourceOperatorFrom(
-      onNext(({ path, query, fragment }) => {
-        const uri = path + query + fragment;
-        history.pushState(undefined, "", uri);
-      }),
-    ),
-    mapDispatch(requestMapper),
-  );
-
-export const create = (priority?: number): React.ComponentType<RouterProps> =>
-  createRouter(() => createRelativeURILocation(priority));
+    return stateContainer;
+  });
