5a17f6d4d33cd2b0f0915fb558583669530828d4
David Bordoley <bordoley@gmail.com>
2019-11-28 19:26:12 -0500

    remove subscriber.observe

--- packages/rx-subscriber/src/index.ts
+++ packages/rx-subscriber/src/index.ts
@@ -6,5 +6,4 @@ export {
 export { toSafeObserver } from "./internal/safeObserver";
 export { createAutoDisposing } from "./internal/autoDisposingSubscriber";
 export { DelegatingSubscriber } from "./internal/delegatingSubscriber";
-export { observe } from "./internal/observe";
 export { pipe } from "./internal/pipe";
