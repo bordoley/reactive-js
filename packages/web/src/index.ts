c82d15bb670e76d6ba35348e17b9c0f9e484f9da
David Bordoley <bordoley@gmail.com>
2019-12-02 03:25:00 -0500

    add a web scheduler as well

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,2 +1,3 @@
 export { fromEvent } from "./internal/event";
 export { createLocationStore, Location } from "./internal/locationStore";
+export { createSchedulerWithPriority } from "./internal/scheduler";
