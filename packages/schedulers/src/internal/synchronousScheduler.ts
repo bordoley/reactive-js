988871d607af32d1def9d76d42f1b30ca2bc6270
David Bordoley <bordoley@gmail.com>
2019-12-13 06:56:24 -0500

    use the abstract virtual time scheduler implementation

--- packages/schedulers/src/internal/synchronousScheduler.ts
+++ packages/schedulers/src/internal/synchronousScheduler.ts
@@ -4,23 +4,12 @@ import {
 } from "./virtualTimeScheduler";
 import {
   DisposableLike,
-  throwIfDisposed,
   createDisposable,
 } from "@reactive-js/disposable";
 
 // Intentionally defined as a module function for perf reasons.
 const shouldYieldFalse = () => false;
 const shouldYieldTrue = () => true;
-
-const iteratorYield = {
-  done: false,
-  value: undefined,
-};
-
-const iteratorDone = {
-  done: true,
-  value: undefined,
-};
 
 // @ts-ignore override shouldYield for perf
 class SynchronousSchedulerResource extends AbstractVirtualTimeSchedulerResource
@@ -41,31 +30,22 @@ class SynchronousSchedulerResource extends AbstractVirtualTimeSchedulerResource
     return createDisposable();
   }
 
-  next(): IteratorResult<void> {
-    throwIfDisposed(this);
-
-    this.shouldYield = shouldYieldTrue;
-    if (this.queue.length > 0) {
-      const next = this.queue.shift() as () => void;
+  protected step(): boolean {
+    const next = this.queue.shift();
+    if (next !== undefined) {
       next();
     }
+    return this.queue.length > 0;
+  }
 
-    return this.queue.length > 0 ? iteratorYield : iteratorDone;
+  next(): IteratorResult<void> {
+    this.shouldYield = shouldYieldTrue;
+    return super.next();
   }
 
   run() {
-    throwIfDisposed(this);
     this.shouldYield = shouldYieldFalse;
-
-    for (
-      let next = this.queue.shift();
-      next !== undefined;
-      next = this.queue.shift()
-    ) {
-      next();
-    }
-
-    this.dispose();
+    super.run();
   }
 }
 
