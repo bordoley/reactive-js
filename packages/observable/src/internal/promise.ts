5328d8d463c7618a6add5420f01297a003466681
David Bordoley <bordoley@gmail.com>
2019-12-14 23:08:09 -0500

    use a class for the ToPromise observer

--- packages/observable/src/internal/promise.ts
+++ packages/observable/src/internal/promise.ts
@@ -3,11 +3,12 @@ import {
   ObserverLike,
   subscribe,
   createObservable,
+  ErrorLike,
 } from "@reactive-js/rx";
 import { SchedulerLike } from "@reactive-js/scheduler";
 import { observe } from "./observe";
 import { pipe, OperatorLike } from "@reactive-js/pipe";
-import { createDisposable } from "@reactive-js/disposable";
+import { createDisposable, DisposableLike, createSerialDisposable } from "@reactive-js/disposable";
 
 export const fromPromiseFactory = <T>(
   factory: () => Promise<T>,
@@ -37,29 +38,55 @@ export const fromPromiseFactory = <T>(
   return createObservable(onSubscribe);
 };
 
+class ToPromiseObserver<T> implements ObserverLike<T> {
+  private result: [T] | undefined = undefined;
+
+  private readonly subscription: DisposableLike;
+  private readonly resolve: (value?: T | PromiseLike<T>) => void;
+  private readonly reject: (reason?: any) => void;
+
+  constructor(
+    subscription: DisposableLike,
+    resolve: (value?: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void,
+  ) {
+    this.subscription = subscription;
+    this.resolve = resolve;
+    this.reject = reject;
+  }
+
+  next(x: T) {
+    if (this.result === undefined) {
+      this.result = [x];
+    } else {
+      this.result[0] = x;
+    }
+  }
+
+  complete(err?: ErrorLike) {
+    this.subscription.dispose();
+    if (err !== undefined) {
+      const { cause } = err;
+      this.reject(cause);
+    } else if (this.result === undefined) {
+      this.reject(new Error("Observable completed without producing a value"));
+    } else {
+      const value = this.result[0];
+      this.resolve(value);
+    }
+  };
+}
+
 export const toPromise = <T>(
   scheduler: SchedulerLike,
 ): OperatorLike<ObservableLike<T>, Promise<T>> => observable =>
   new Promise((resolve, reject) => {
-    let result: T | undefined = undefined;
-    const subscription = pipe(
+    const subscription = createSerialDisposable();
+    const observer = new ToPromiseObserver(subscription, resolve, reject);
+
+    subscription.disposable = pipe(
       observable,
-      observe({
-        next: v => {
-          result = v;
-        },
-        complete: err => {
-          subscription.dispose();
-          if (err !== undefined) {
-            const { cause } = err;
-            reject(cause);
-          } else if (result === undefined) {
-            reject(new Error("Observable completed without producing a value"));
-          } else {
-            resolve(result);
-          }
-        },
-      }),
+      observe(observer),
       subscribe(scheduler),
     );
   });
