8d93866e2cdb794ad8ccf6d5acaf4a7ca4ed6b4f
David Bordoley <bordoley@gmail.com>
2019-12-21 10:16:16 -0500

    [switchAll] unify subscriber and observer onto the same instance

--- packages/observable/src/internal/switch.ts
+++ packages/observable/src/internal/switch.ts
@@ -12,41 +12,25 @@ import { lift } from "./lift";
 import { observe } from "./observe";
 import { pipe } from "@reactive-js/pipe";
 
-class SwitchSubscriber<T> extends AbstractDelegatingSubscriber<
-  ObservableLike<T>,
-  T
-> {
-  static InnerObserver = class<T> implements ObserverLike<T> {
-    private readonly parent: SwitchSubscriber<T>;
-    constructor(parent: SwitchSubscriber<T>) {
-      this.parent = parent;
-    }
-
-    onComplete(error?: ErrorLike) {
-      if (error !== undefined) {
-        this.parent.complete(error);
-      }
-    }
-
-    onNext(data: T) {
-      this.parent.delegate.next(data);
-    }
-  };
-
+class SwitchSubscriber<T>
+  extends AbstractDelegatingSubscriber<ObservableLike<T>, T>
+  implements ObserverLike<T> {
   private innerSubscription = disposed;
-
-  constructor(delegate: SubscriberLike<T>) {
-    super(delegate);
-  }
 
   next(data: ObservableLike<T>) {
     this.remove(this.innerSubscription);
-    this.innerSubscription = pipe(
-      data,
-      observe(new SwitchSubscriber.InnerObserver(this)),
-      subscribe(this),
-    );
+    this.innerSubscription = pipe(data, observe(this), subscribe(this));
     this.add(this.innerSubscription);
+  }
+
+  onComplete(error?: ErrorLike) {
+    if (error !== undefined) {
+      this.complete(error);
+    }
+  }
+
+  onNext(data: T) {
+    this.delegate.next(data);
   }
 }
 
