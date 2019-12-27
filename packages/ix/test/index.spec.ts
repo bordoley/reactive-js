0e6b3db5f90c2bbfaece4a715642207efaa2c8da
David Bordoley <bordoley@gmail.com>
2019-12-27 10:28:01 -0500

    add catchError operator and properly complete ix fromIterable

--- packages/ix/test/index.spec.ts
+++ packages/ix/test/index.spec.ts
@@ -1,12 +1,12 @@
 import { createVirtualTimeSchedulerResource } from "@reactive-js/schedulers";
 import { fromArray, fromIterable, generate } from "../src/index";
 import { pipe } from "@reactive-js/pipe";
-import { subscribe, onNext } from "@reactive-js/rx";
+import { subscribe, onNext, ErrorLike, onComplete } from "@reactive-js/rx";
 
 test("fromArray", () => {
   const scheduler = createVirtualTimeSchedulerResource();
   const iter = fromArray([1,2,3,4,5,6], scheduler);
-  
+
   const result: number[] = [];
   pipe(iter, onNext(x => result.push(x)), subscribe(scheduler));
 
@@ -21,23 +21,27 @@ test("fromArray", () => {
 test("fromIterable", () => {
   const scheduler = createVirtualTimeSchedulerResource();
   const iter = fromIterable([1,2,3,4,5,6], scheduler);
-  
+
   const result: number[] = [];
-  pipe(iter, onNext(x => result.push(x)), subscribe(scheduler));
+  let error: ErrorLike | undefined = undefined;
+  pipe(iter, onNext(x => result.push(x)), onComplete(e => { error = e}), subscribe(scheduler));
 
   iter.dispatch(1);
   iter.dispatch(2);
+  iter.dispatch(3);
+  iter.dispatch(5);
 
   scheduler.run();
 
-  expect(result).toEqual([1,2,3]);
+  expect(result).toEqual([1,2,3,4,5,6]);
+  expect(error).toBeUndefined();
 })
 
 
 test("generate", () => {
   const scheduler = createVirtualTimeSchedulerResource();
   const iter = generate(x => x + 1, () => 0, scheduler);
-  
+
   const result: number[] = [];
   pipe(iter, onNext(x => result.push(x)), subscribe(scheduler));
 
