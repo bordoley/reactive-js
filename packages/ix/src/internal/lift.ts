4ae8e7b8104fa327dd5d8b151af7b09ec938a0da
David Bordoley <bordoley@gmail.com>
2019-12-27 11:00:26 -0500

    better liftReq contract

--- packages/ix/src/internal/lift.ts
+++ packages/ix/src/internal/lift.ts
@@ -12,7 +12,7 @@ class LiftedIteratorImpl<TReq, T> implements AsyncIteratorLike<TReq, T> {
   constructor(
     readonly dispatcher: (req: TReq) => void,
     readonly observable: MulticastObservableLike<T>,
-  ) {}
+  ) { }
 
   get subscriberCount(): number {
     return this.observable.subscriberCount;
@@ -27,41 +27,36 @@ class LiftedIteratorImpl<TReq, T> implements AsyncIteratorLike<TReq, T> {
   }
 }
 
-const liftImpl = <TReq, T, TReqA, TA>(
+export const lift = <TReq, T, TA>(
+  operator: OperatorLike<ObservableLike<T>, MulticastObservableLike<TA>>,
   scheduler: SchedulerLike,
-  operator?: OperatorLike<ObservableLike<T>, ObservableLike<TA>>,
-  dispatchOperator?: (dispatcher: (req: TReq) => void) => (req: TReqA) => void,
   replay?: number,
-): AsyncIteratorOperatorLike<TReq, T, TReqA, TA> => iterator => {
-  const observable: ObservableLike<T> =
-    (iterator as any).observable || iterator;
+): AsyncIteratorOperatorLike<TReq, T, TReq, TA> => iterator => {
+  const observable: ObservableLike<T> = (iterator as any).observable || iterator;
   const dispatcher: (req: TReq) => void =
     (iterator as any).dispatcher || ((req: any) => iterator.dispatch(req));
 
   const liftedObservable =
     operator !== undefined ? operator(observable) : observable;
 
-  const liftedDispatcher: (req: TReqA) => void =
-    dispatchOperator !== undefined
-      ? dispatchOperator(dispatcher)
-      : (dispatcher as any);
-
   return new LiftedIteratorImpl(
-    liftedDispatcher,
+    dispatcher,
     pipe(liftedObservable as ObservableLike<TA>, share(scheduler, replay)),
   );
-};
-
-export const lift = <TReq, T, TA>(
-  operator: OperatorLike<ObservableLike<T>, MulticastObservableLike<TA>>,
-  scheduler: SchedulerLike,
-  replay?: number,
-): AsyncIteratorOperatorLike<TReq, T, TReq, TA> =>
-  liftImpl(scheduler, operator, undefined, replay);
+}
 
 export const liftReq = <TReq, T, TReqA>(
   operator: (dispatcher: (req: TReq) => void) => (ref: TReqA) => void,
-  scheduler: SchedulerLike,
-  replay?: number,
-): AsyncIteratorOperatorLike<TReq, T, TReqA, T> =>
-  liftImpl(scheduler, undefined, operator, replay);
+): AsyncIteratorOperatorLike<TReq, T, TReqA, T> => iterator => {
+  const observable: MulticastObservableLike<T> = (iterator as any).observable || iterator;
+
+  const dispatcher: (req: TReq) => void =
+    (iterator as any).dispatcher || ((req: any) => iterator.dispatch(req));
+
+  const liftedDispatcher: (req: TReqA) => void = operator(dispatcher);
+
+  return new LiftedIteratorImpl(
+    liftedDispatcher,
+    observable,
+  );
+}
