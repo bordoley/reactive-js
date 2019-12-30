6a7d27c9da14f92d8aae4c325b9c7f659e889e5d
David Bordoley <bordoley@gmail.com>
2019-12-30 12:03:04 -0500

    ix cleanups

--- packages/ix/src/internal/identity.ts
+++ packages/ix/src/internal/identity.ts
@@ -1,10 +1,13 @@
 import { AsyncIterableLike } from "./interfaces";
 import { SchedulerLike } from "@reactive-js/scheduler";
-import { createEventEmitter } from "./createAsyncIterator";
+import { AsyncIteratorResourceImpl } from "./createAsyncIterator";
+import { createSubject } from "@reactive-js/rx";
 
 const instance = {
-  getIXAsyncIterator: (_: SchedulerLike, replayCount = 0) =>
-    createEventEmitter(replayCount),
+  getIXAsyncIterator: (_: SchedulerLike, replayCount = 0) => {
+    const dispatcher = createSubject(replayCount);
+    return new AsyncIteratorResourceImpl(dispatcher, dispatcher);
+  }
 };
 
 export const identity = <T>(): AsyncIterableLike<T, T> => instance;
