21b60e7cf6281c905a28ab33163699e6be36a2d3
David Bordoley <bordoley@gmail.com>
2020-01-04 20:29:09 -0500

    subject implements AsyncEnumerator so use it directly

--- packages/ix/src/internal/identity.ts
+++ packages/ix/src/internal/identity.ts
@@ -1,12 +1,10 @@
 import { AsyncEnumerableLike } from "./interfaces";
 import { SchedulerLike } from "@reactive-js/scheduler";
-import { AsyncEnumeratorResourceImpl } from "./createAsyncEnumerator";
 import { createSubject } from "@reactive-js/rx";
 
 const instance = {
   enumerateAsync: (scheduler: SchedulerLike, replayCount = 0) => {
-    const subject = createSubject(scheduler, replayCount);
-    return new AsyncEnumeratorResourceImpl(subject, subject);
+    return createSubject(scheduler, replayCount);
   },
 };
 
