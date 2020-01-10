fe20e0639f5b0bb17a826a3947425a66a6c0b65f
David Bordoley <bordoley@gmail.com>
2020-01-10 17:49:07 -0500

    add more missing methods

--- packages/ix/src/internal/disposedAsyncEnumerator.ts
+++ packages/ix/src/internal/disposedAsyncEnumerator.ts
@@ -3,9 +3,11 @@ import { disposed as disposedDisposable } from "@reactive-js/disposable";
 
 const _disposed: AsyncEnumeratorResourceLike<unknown, any> = {
   ...(disposedDisposable as any),
+  now: 0,
   subscriberCount: 0,
-  notify: _ => {},
-  notifySafe: _ => {},
+  notify(_) {},
+  notifySafe(_){},
+  schedule: _ => disposedDisposable,
   subscribe: subscriber => subscriber.dispose(),
 };
 
