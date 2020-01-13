64d6b71b4de722d01c317ab4a719430177acc819
David Bordoley <bordoley@gmail.com>
2020-01-13 15:04:54 -0500

    tweak bindNodeCallback to always take a selector

--- packages/node/src/internal/bindNodeCallback.ts
+++ packages/node/src/internal/bindNodeCallback.ts
@@ -1,83 +1,95 @@
 import { createObservable, ObservableLike } from "@reactive-js/rx";
 
-export function bindNodeCallback<R1, R2, R3, R4>(
+export function bindNodeCallback<R1, R2, R3, R4, T>(
   callbackFunc: (
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
-): () => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<R1, R2, R3>(
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T
+): () => ObservableLike<T>;
+export function bindNodeCallback<R1, R2, R3, T>(
   callbackFunc: (
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
-): () => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<R1, R2>(
+  selector: (r1: R1, r2: R2, r3: R3) => T
+): () => ObservableLike<T>;
+export function bindNodeCallback<R1, R2, T>(
   callbackFunc: (
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
-): () => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<R1>(
+  selector: (r1: R1, r2: R2) => T
+): () => ObservableLike<T>;
+export function bindNodeCallback<R1, T>(
   callbackFunc: (callback: (err: unknown, res1: R1) => unknown) => unknown,
-): () => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): () => ObservableLike<T>;
 export function bindNodeCallback(
   callbackFunc: (callback: (err: unknown) => unknown) => unknown,
 ): () => ObservableLike<void>;
 
-export function bindNodeCallback<A1, R1, R2, R3, R4>(
+export function bindNodeCallback<A1, R1, R2, R3, R4, T>(
   callbackFunc: (
     arg1: A1,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
-): (arg1: A1) => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<A1, R1, R2, R3>(
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T
+): (arg1: A1) => ObservableLike<T>;
+export function bindNodeCallback<A1, R1, R2, R3, T>(
   callbackFunc: (
     arg1: A1,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
-): (arg1: A1) => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<A1, R1, R2>(
+  selector: (r1: R1, r2: R2, r3: R3) => T
+): (arg1: A1) => ObservableLike<T>;
+export function bindNodeCallback<A1, R1, R2, T>(
   callbackFunc: (
     arg1: A1,
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
-): (arg1: A1) => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<A1, R1>(
+  selector: (r1: R1, r2: R2) => T
+): (arg1: A1) => ObservableLike<T>;
+export function bindNodeCallback<A1, R1, T>(
   callbackFunc: (
     arg1: A1,
     callback: (err: unknown, res1: R1) => unknown,
   ) => unknown,
-): (arg1: A1) => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): (arg1: A1) => ObservableLike<T>;
 export function bindNodeCallback<A1>(
   callbackFunc: (arg1: A1, callback: (err: unknown) => unknown) => unknown,
 ): (arg1: A1) => ObservableLike<void>;
 
-export function bindNodeCallback<A1, A2, R1, R2, R3, R4>(
+export function bindNodeCallback<A1, A2, R1, R2, R3, R4, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2) => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<A1, A2, R1, R2, R3>(
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T,
+): (arg1: A1, arg2: A2) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, R1, R2, R3, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2) => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<A1, A2, R1, R2>(
+  selector: (r1: R1, r2: R2, r3: R3,) => T
+): (arg1: A1, arg2: A2) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, R1, R2, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2) => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<A1, A2, R1>(
+  selector: (r1: R1, r2: R2) => T
+): (arg1: A1, arg2: A2) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, R1, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     callback: (err: unknown, res1: R1) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2) => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): (arg1: A1, arg2: A2) => ObservableLike<T>;
 export function bindNodeCallback<A1, A2>(
   callbackFunc: (
     arg1: A1,
@@ -86,38 +98,42 @@ export function bindNodeCallback<A1, A2>(
   ) => unknown,
 ): (arg1: A1, arg2: A2) => ObservableLike<void>;
 
-export function bindNodeCallback<A1, A2, A3, R1, R2, R3, R4>(
+export function bindNodeCallback<A1, A2, A3, R1, R2, R3, R4, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     arg3: A3,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<A1, A2, A3, R1, R2, R3>(
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T
+): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, R1, R2, R3, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     arg3: A3,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<A1, A2, A3, R1, R2>(
+  selector: (r1: R1, r2: R2, r3: R3) => T
+): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, R1, R2, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     arg3: A3,
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<A1, A2, A3, R1>(
+  selector: (r1: R1, r2: R2) => T
+): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, R1, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
     arg3: A3,
     callback: (err: unknown, res1: R1) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<T>;
 export function bindNodeCallback<A1, A2, A3>(
   callbackFunc: (
     arg1: A1,
@@ -127,7 +143,7 @@ export function bindNodeCallback<A1, A2, A3>(
   ) => unknown,
 ): (arg1: A1, arg2: A2, arg3: A3) => ObservableLike<void>;
 
-export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3, R4>(
+export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3, R4, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -135,8 +151,9 @@ export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3, R4>(
     arg4: A4,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3>(
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T,
+): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -144,8 +161,9 @@ export function bindNodeCallback<A1, A2, A3, A4, R1, R2, R3>(
     arg4: A4,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<A1, A2, A3, A4, R1, R2>(
+  selector: (r1: R1, r2: R2, r3: R3) => T
+): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, R1, R2, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -153,8 +171,9 @@ export function bindNodeCallback<A1, A2, A3, A4, R1, R2>(
     arg4: A4,
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<A1, A2, A3, A4, R1>(
+  selector: (r1: R1, r2: R2) => T,
+): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, R1, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -162,7 +181,8 @@ export function bindNodeCallback<A1, A2, A3, A4, R1>(
     arg4: A4,
     callback: (err: unknown, res1: R1) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<T>;
 export function bindNodeCallback<A1, A2, A3, A4>(
   callbackFunc: (
     arg1: A1,
@@ -173,7 +193,7 @@ export function bindNodeCallback<A1, A2, A3, A4>(
   ) => unknown,
 ): (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => ObservableLike<void>;
 
-export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3, R4>(
+export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3, R4, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -182,14 +202,15 @@ export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3, R4>(
     arg5: A5,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3, res4: R4) => unknown,
   ) => unknown,
+  selector: (r1: R1, r2: R2, r3: R3, r4: R4) => T,
 ): (
   arg1: A1,
   arg2: A2,
   arg3: A3,
   arg4: A4,
   arg5: A5,
-) => ObservableLike<[R1, R2, R3, R4]>;
-export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3>(
+) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -198,14 +219,15 @@ export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, R3>(
     arg5: A5,
     callback: (err: unknown, res1: R1, res2: R2, res3: R3) => unknown,
   ) => unknown,
+  selector: (r1: R1, r2: R2, r3: R3) => T
 ): (
   arg1: A1,
   arg2: A2,
   arg3: A3,
   arg4: A4,
   arg5: A5,
-) => ObservableLike<[R1, R2, R3]>;
-export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2>(
+) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -214,14 +236,15 @@ export function bindNodeCallback<A1, A2, A3, A4, A5, R1, R2>(
     arg5: A5,
     callback: (err: unknown, res1: R1, res2: R2) => unknown,
   ) => unknown,
+  selector: (r1: R1, r2: R2) => T
 ): (
   arg1: A1,
   arg2: A2,
   arg3: A3,
   arg4: A4,
   arg5: A5,
-) => ObservableLike<[R1, R2]>;
-export function bindNodeCallback<A1, A2, A3, A4, A5, R1>(
+) => ObservableLike<T>;
+export function bindNodeCallback<A1, A2, A3, A4, A5, R1, T>(
   callbackFunc: (
     arg1: A1,
     arg2: A2,
@@ -230,7 +253,8 @@ export function bindNodeCallback<A1, A2, A3, A4, A5, R1>(
     arg5: A5,
     callback: (err: unknown, res1: R1) => unknown,
   ) => unknown,
-): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => ObservableLike<R1>;
+  selector: (r1: R1) => T
+): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => ObservableLike<T>;
 export function bindNodeCallback<A1, A2, A3, A4, A5>(
   callbackFunc: (
     arg1: A1,
@@ -242,19 +266,21 @@ export function bindNodeCallback<A1, A2, A3, A4, A5>(
   ) => unknown,
 ): (arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => ObservableLike<void>;
 
-export function bindNodeCallback(
+export function bindNodeCallback<T>(
   callback: Function,
-): (...args: unknown[]) => ObservableLike<unknown> {
+  selector?: (...args: unknown[]) => T
+): (...args: unknown[]) => ObservableLike<T | void> {
   return function(this: unknown, ...args: unknown[]) {
     return createObservable(subscriber => {
       const handler = (cause: unknown, ...innerArgs: unknown[]) => {
         if (cause) {
           subscriber.dispose({ cause });
         } else {
-          if (innerArgs.length > 1) {
-            subscriber.dispatch(innerArgs);
-          } else if (innerArgs.length === 1) {
-            subscriber.dispatch(innerArgs[0]);
+          if (innerArgs.length > 0) {
+            const result = (selector as (...args: unknown[]) => T)(...innerArgs);
+            subscriber.dispatch(result);
+          } else {
+            subscriber.dispatch(undefined);
           }
           subscriber.dispose();
         }
