ef6b448c3dd34282c0df11d8e2c84548b553d2f8
David Bordoley <bordoley@gmail.com>
2020-03-28 12:18:07 -0400

    remove commented out code

--- packages/enumerable/src/index.ts
+++ packages/enumerable/src/index.ts
@@ -1,3 +1,1 @@
 export { EnumeratorLike, EnumerableLike } from "./internal/interfaces";
-//export { AbstractDelegatingEnumerator } from "./internal/enumerator";
-//export { map } from "./internal/map";
