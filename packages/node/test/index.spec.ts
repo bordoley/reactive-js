28882ffc5158bb1156eef968136000c48aba754d
David Bordoley <bordoley@gmail.com>
2020-03-30 15:34:32 -0400

    add createWritableAsyncEnumerable to node package. refactor createReadableAsyncEnumerable. add tests.

--- packages/node/test/index.spec.ts
+++ packages/node/test/index.spec.ts
@@ -1,8 +1,94 @@
-import { Readable } from "stream";
-import { reduce, ReducerRequestType } from "@reactive-js/async-enumerable";
+import { Readable, Writable } from "stream";
+import {
+  reduce,
+  ReducerRequestType,
+  createAsyncEnumerable,
+  sink,
+} from "@reactive-js/async-enumerable";
 import { pipe } from "@reactive-js/pipe";
-import { toPromise } from "@reactive-js/observable";
-import { createReadableAsyncEnumerable, getHostScheduler } from "../src";
+import {
+  toPromise,
+  ObservableLike,
+  map,
+  scan,
+  takeWhile,
+} from "@reactive-js/observable";
+import {
+  createReadableAsyncEnumerable,
+  getHostScheduler,
+  createWritableAsyncEnumerable,
+} from "../src";
+import { StringDecoder } from "string_decoder";
+
+describe("writable", () => {
+  test("sinking to the buffer", async () => {
+    let data = "";
+    const decoder = new StringDecoder();
+
+    const writable = new Writable({
+      highWaterMark: 4,
+
+      write(chunk, encoding, callback) {
+        if (encoding === "buffer") {
+          chunk = decoder.write(chunk);
+        }
+        data += chunk;
+        callback();
+      },
+    });
+
+    const dest = createWritableAsyncEnumerable(() => writable);
+    const src = createAsyncEnumerable((src: ObservableLike<void>) =>
+      pipe(
+        src,
+        scan(
+          (acc, _) => {
+            switch (acc) {
+              case "":
+                return "abc";
+              case "abc":
+                return "defg";
+              default:
+                return "done";
+            }
+          },
+          () => "",
+        ),
+        takeWhile(x => x !== "done"),
+        map(x => Buffer.from(x, "utf8")),
+      ),
+    );
+
+    await pipe(
+      sink(src, dest),
+      toPromise(getHostScheduler()),
+    );
+    expect(data).toEqual("abcdefg");
+  });
+  test("when the write throws an exception", async () => {
+    const cause = new Error();
+
+    const writable = new Writable({
+      write(_chunk, _encoding, callback) {
+        callback(cause);
+      },
+    });
+
+    const dest = createWritableAsyncEnumerable(() => writable);
+    const src = createAsyncEnumerable(src =>
+      pipe(
+        src,
+        map(_ => Buffer.from("a", "utf8")),
+      ),
+    );
+
+    const promise = pipe(
+      sink(src, dest),
+      toPromise(getHostScheduler()),
+    );
+    expect(promise).rejects.toThrow(cause);
+  });
+});
 
 describe("createReadableAsyncEnumerable", () => {
   test("when the readable is infinite", async () => {
