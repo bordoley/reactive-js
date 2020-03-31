5276c20973d08e5ab8deaac9cb30ebdb9ac1d36a
David Bordoley <bordoley@gmail.com>
2020-03-31 01:45:24 -0400

    add transform operator to node package

--- packages/async-enumerable/src/internal/sink.ts
+++ packages/async-enumerable/src/internal/sink.ts
@@ -1,20 +1,25 @@
 import { AsyncEnumerableLike } from "./interfaces";
-import { ObservableLike, createObservable, endWith } from "@reactive-js/observable";
+import {
+  ObservableLike,
+  createObservable,
+  endWith,
+} from "@reactive-js/observable";
 import { pipe } from "@reactive-js/pipe";
 
 export const sink = <TReq, T>(
   src: AsyncEnumerableLike<TReq, T>,
   dest: AsyncEnumerableLike<T, TReq>,
-): ObservableLike<void> => pipe(
-  createObservable<void>(subscriber => {
-    const destEnumerator = dest.enumerateAsync(subscriber);
-    const srcEnumerator = src.enumerateAsync(subscriber);
+): ObservableLike<void> =>
+  pipe(
+    createObservable<void>(subscriber => {
+      const destEnumerator = dest.enumerateAsync(subscriber);
+      const srcEnumerator = src.enumerateAsync(subscriber);
 
-    srcEnumerator.subscribe(destEnumerator);
-    destEnumerator.subscribe(srcEnumerator);
+      srcEnumerator.subscribe(destEnumerator);
+      destEnumerator.subscribe(srcEnumerator);
 
-    subscriber.add(destEnumerator).add(srcEnumerator);
-    destEnumerator.add(subscriber);
-  }),
-  endWith(undefined as void),
-);
+      subscriber.add(destEnumerator).add(srcEnumerator);
+      destEnumerator.add(subscriber);
+    }),
+    endWith(undefined as void),
+  );
