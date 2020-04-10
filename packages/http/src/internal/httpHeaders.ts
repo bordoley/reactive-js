6da048f1078709478342436adb60776f2efb042f
David Bordoley <bordoley@gmail.com>
2020-04-10 15:05:59 -0400

    add an enum of standard and ext http headers.

--- packages/http/src/internal/httpHeaders.ts
+++ packages/http/src/internal/httpHeaders.ts
@@ -1,31 +1,121 @@
 import { HttpHeadersLike } from "./interfaces";
 
+export const enum HttpStandardHeader {
+  Accept = "Accept",
+  AcceptCharset = "Accept-Charset",
+  AcceptEncoding = "Accept-Encoding",
+  AcceptLanguage = "Accept-Language",
+  AcceptRange = "Accept-Ranges",
+  Age = "Age",
+  Allow = "Allow",
+  Authorization = "Authorization",
+  CacheControl = "Cache-Control",
+  Connection = "Connection",
+  ContentEncoding = "Content-Encoding",
+  ContentLanguage = "Content-Language",
+  ContentLength = "Content-Length",
+  ContentLocation = "Content-Location",
+  ContentMD5 = "Content-MD5",
+  ContentRange = "Content-Range",
+  ContentType = "Content-Type",
+  Cookie = "Cookie",
+  Date = "Date",
+  ETag = "ETag",
+  Expect = "Expect",
+  Expires = "Expires",
+  From = "From",
+  Host = "Host",
+  IfMatch = "If-Match",
+  IfModifiedSince = "If-Modified-Since",
+  IfNoneMatch = "If-None-Match",
+  IfRange = "If-Range",
+  IfUnmodifiedSince = "If-Unmodified-Since",
+  LastModified = "Last-Modified",
+  Location = "Location",
+  MaxForwards = "Max-Forwards",
+  Pragma = "Pragma",
+  ProxyAuthenticate = "Proxy-Authenticate",
+  ProxyAuthorization = "Proxy-Authorization",
+  Range = "Range",
+  Referer = "Referer",
+  RetryAfter = "Retry-After",
+  Server = "Server",
+  SetCookie = "Set-Cookie",
+  TE = "TE",
+  railer = "Trailer",
+  TransferEncoding = "Transfer-Encoding",
+  Upgrade = "Upgrade",
+  UserAgent = "User-Agent",
+  Vary = "Vary",
+  Via = "Via",
+  Warning = "Warning",
+  WWWAuthenticate = "WWW-Authenticate",
+}
+
+export const enum HttpExtensiondHeader {
+  XForwardedProto = "X-Forwarded-Proto",
+  XForwardedHost = "X-Forwarded-Host",
+  XHttpMethod = "X-HTTP-Method",
+  XHttpMethodOverride = "X-HTTP-Method-Override",
+  XMethodOverride = "X-Method-Override",
+}
+
 const bannedHeaders = [
-  "accept-charset",
-  "accept-encoding",
-  "accept-language",
-  "accept",
-  "content-encoding",
-  "content-length",
-  "content-type",
-  "etag",
-  "expect",
-  "expires",
-  "if-match",
-  "if-none-match",
-  "if-modified-since",
-  "if-unmodified-since",
-  "if-range",
-  "last-modified",
-  "transfer-encoding",
-  "vary",
-];
+  HttpStandardHeader.Accept,
+  HttpStandardHeader.AcceptCharset,
+  HttpStandardHeader.AcceptEncoding,
+  HttpStandardHeader.AcceptLanguage,
+  HttpStandardHeader.ContentEncoding,
+  HttpStandardHeader.ContentLength,
+  HttpStandardHeader.ContentType,
+  HttpStandardHeader.ETag,
+  HttpStandardHeader.Expect,
+  HttpStandardHeader.Expires,
+  HttpStandardHeader.IfMatch,
+  HttpStandardHeader.IfNoneMatch,
+  HttpStandardHeader.IfModifiedSince,
+  HttpStandardHeader.IfUnmodifiedSince,
+  HttpStandardHeader.IfRange,
+  HttpStandardHeader.LastModified,
+  HttpStandardHeader.TransferEncoding,
+  HttpStandardHeader.Vary,
+].map(s => s.toLowerCase());
+
+export function getHeaderValue(
+  headers: HttpHeadersLike,
+  key: HttpStandardHeader,
+): string;
+
+export function getHeaderValue(
+  headers: HttpHeadersLike,
+  key: HttpExtensiondHeader,
+): string;
+
+export function getHeaderValue(
+  headers: HttpHeadersLike,
+  key: HttpStandardHeader,
+  orDefault: string,
+): string;
+
+export function getHeaderValue(
+  headers: HttpHeadersLike,
+  key: HttpExtensiondHeader,
+  orDefault: string,
+): string;
+
+export function getHeaderValue(
+  headers: HttpHeadersLike,
+  key: string,
+  orDefault?: string,
+): string | undefined {
+  return headers[key.toLowerCase()] || orDefault;
+}
 
 /** @ignore */
-export const writeHttpHeaders = (
+export function writeHttpHeaders(
   headers: HttpHeadersLike,
   writeHeader: (header: string, value: string) => void,
-) => {
+) {
   const headerPairs = Object.entries(headers).filter(
     ([key]) => !bannedHeaders.includes(key.toLowerCase()),
   );
@@ -33,4 +123,4 @@ export const writeHttpHeaders = (
   for (const [header, value] of headerPairs) {
     writeHeader(header, value);
   }
-};
+}
