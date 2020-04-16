d4c3f80462c5e1d5c483906d0cd15efa5426c176
David Bordoley <bordoley@gmail.com>
2020-04-16 17:52:17 -0400

    cleaner enumerator impl

--- packages/enumerable/src/internal/toIterable.ts
+++ packages/enumerable/src/internal/toIterable.ts
@@ -1,40 +1,17 @@
-import { EnumerableLike, EnumeratorLike } from "./interfaces";
-import { DisposableLike } from "@reactive-js/disposable";
-
-const done: IteratorResult<any> = { done: true, value: undefined };
-
-class EnumeratorIterator<T> implements Iterator<T> {
-  constructor(
-    private readonly enumerator: EnumeratorLike<void, T> & DisposableLike,
-  ) {}
-
-  next<T>(): IteratorResult<T> {
-    const enumerator = this.enumerator;
-    return enumerator.move()
-      ? { done: false, value: enumerator.current }
-      : done;
-  }
-
-  return<T>(value?: any): IteratorResult<T> {
-    this.enumerator.dispose();
-    return value !== undefined ? { done: true, value } : done;
-  }
-
-  throw<T>(error?: unknown): IteratorResult<T> {
-    this.enumerator.dispose();
-    if (error !== undefined) {
-      throw error;
-    }
-    return done;
-  }
-}
+import { EnumerableLike } from "./interfaces";
 
 class EnumerableIterable<T> implements Iterable<T> {
   constructor(private readonly enumerable: EnumerableLike<void, T>) {}
 
-  [Symbol.iterator]() {
+  *[Symbol.iterator]() {
     const enumerator = this.enumerable.enumerate();
-    return new EnumeratorIterator(enumerator);
+    try {
+      while (enumerator.move()) {
+        yield enumerator.current;
+      }
+    } finally {
+      enumerator.dispose();
+    }
   }
 }
 
