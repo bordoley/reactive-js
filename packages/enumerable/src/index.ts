03ed6a9a1de6f63489b29f98585c868e0d69f245
David Bordoley <bordoley@gmail.com>
2020-04-16 22:34:36 -0400

    add forEach

--- packages/enumerable/src/index.ts
+++ packages/enumerable/src/index.ts
@@ -1,3 +1,4 @@
+export { forEach } from "./internal/forEach";
 export { fromIterable, fromIterator } from "./internal/fromIterator";
 export { EnumeratorLike, EnumerableLike } from "./internal/interfaces";
 export { toIterable } from "./internal/toIterable";
---
+++ packages/enumerable/src/internal/forEach.ts
@@ -0,0 +1,17 @@
+import { Operator } from "@reactive-js/pipe";
+
+import { EnumerableLike } from "./interfaces"
+
+export const forEach = <T>(
+  f: (v:T) => void
+): Operator<EnumerableLike<void, T>, void> => enumerable => {
+  const enumerator = enumerable.enumerate();
+  try {
+    while(enumerator.move()) {
+      f(enumerator.current);
+    }
+    enumerator.dispose();
+  } catch(e) {
+    enumerator.dispose(e);
+  }
+}
