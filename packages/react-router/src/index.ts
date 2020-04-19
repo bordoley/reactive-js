05d4d41244d7d8972f71eec29a7684020e8f14ca
David Bordoley <bordoley@gmail.com>
2020-04-19 12:55:12 -0400

    fix #122

--- packages/react-router/src/index.ts
+++ packages/react-router/src/index.ts
@@ -1,2 +1,2 @@
-export { RoutableComponentProps, RouterProps, Router } from "./internal/router";
+export { RelativeURI, RoutableComponentProps, RouterProps, Router } from "./internal/router";
 export { useRoutableState } from "./internal/useRoutableState";
