e9875be932cc186ccbbb27496a618cfc17637fab
David Bordoley <bordoley@gmail.com>
2020-04-20 00:19:33 -0400

    remove createBufferStream*AsyncEnumerator functions

--- packages/node/src/internal/bufferStream.ts
+++ packages/node/src/internal/bufferStream.ts
@@ -2,123 +2,63 @@ import iconv from "iconv-lite";
 import { Readable } from "stream";
 import {
   createAsyncEnumerable,
-  createAsyncEnumerator,
-  AsyncEnumeratorLike,
-  StreamEvent,
   StreamEventType,
   StreamMode,
   ofValueStream,
 } from "@reactive-js/async-enumerable";
 import { createDisposableValue } from "@reactive-js/disposable";
-import {
-  AbstractDelegatingSubscriber,
-  SafeSubscriberLike,
-  toSafeSubscriber,
-  lift,
-  ObservableOperator,
-  SubscriberOperator,
-  using,
-  assertSubscriberNotifyInContinuation,
-} from "@reactive-js/observable";
+import { createObservable, onNotify, subscribe } from "@reactive-js/observable";
 import { pipe, Operator } from "@reactive-js/pipe";
-import { SchedulerLike } from "@reactive-js/scheduler";
 import { BufferStreamLike } from "./interfaces";
 
-class ReadableStreamModeSubscriber extends AbstractDelegatingSubscriber<
-  StreamMode,
-  StreamEvent<Buffer>
-> {
-  constructor(
-    delegate: SafeSubscriberLike<StreamEvent<Buffer>>,
-    private readonly readable: Readable,
-  ) {
-    super(delegate);
-    this.add(delegate);
-  }
-
-  notify(data: StreamMode) {
-    assertSubscriberNotifyInContinuation(this);
-
-    const readable = this.readable;
-    switch (data) {
-      case StreamMode.Pause:
-        readable.pause();
-        break;
-      case StreamMode.Produce:
-        readable.resume();
-        break;
-    }
-  }
-}
-
-const subscriberOperator = (
-  readable: Readable,
-): SubscriberOperator<StreamMode, StreamEvent<Buffer>> => subscriber => {
-  const safeSubscriber = toSafeSubscriber(subscriber).add(() => {
-    readable.pause();
-    readable.removeListener("data", onData);
-    readable.removeListener("end", onEnd);
-    readable.removeListener("error", onError);
-  });
-
-  const onData = (data: Buffer) => {
-    safeSubscriber.dispatch({ type: StreamEventType.Next, data });
-  };
-  readable.on("data", onData);
-
-  const onEnd = () => {
-    safeSubscriber.dispatch({ type: StreamEventType.Complete });
-    // Intentionally don't dispose the subscriber,
-    // because it may be asynchronously consuming
-    // the data.
-  };
-  readable.on("end", onEnd);
-
-  const onError = (cause: any) => {
-    safeSubscriber.dispose({ cause });
-  };
-  readable.on("error", onError);
-
-  readable.pause();
-
-  return new ReadableStreamModeSubscriber(safeSubscriber, readable);
-};
-
-const operator = (readable: Readable) =>
-  lift(subscriberOperator(readable), false);
-
-export const createBufferStreamAsyncEnumeratorFromReadable = (
-  readable: Readable,
-  scheduler: SchedulerLike,
-  replayCount?: number,
-): AsyncEnumeratorLike<StreamMode, StreamEvent<Buffer>> =>
-  createAsyncEnumerator(operator(readable), scheduler, replayCount);
-
 const disposeReadable = (readable: Readable) => {
+  readable.removeAllListeners();
   readable.destroy();
 };
 
-const observableOperator = (
-  factory: () => Readable,
-): ObservableOperator<StreamMode, StreamEvent<Buffer>> => observable =>
-  using(
-    _ => {
-      const readable = factory();
-      const disposable = createDisposableValue(readable, disposeReadable);
-
-      const onEnd = () => {
-        disposable.dispose();
-      };
-      readable.on("end", onEnd);
-
-      return disposable;
-    },
-    readable => pipe(observable, operator(readable.value)),
-  );
-
 export const createBufferStreamFromReadable = (
   factory: () => Readable,
-): BufferStreamLike => createAsyncEnumerable(observableOperator(factory));
+): BufferStreamLike =>
+  createAsyncEnumerable(mode =>
+    createObservable(subscriber => {
+      const readable = createDisposableValue(factory(), disposeReadable);
+
+      const modeSubscription = pipe(
+        mode,
+        onNotify(ev => {
+          switch (ev) {
+            case StreamMode.Pause:
+              readable.value.pause();
+              break;
+            case StreamMode.Produce:
+              readable.value.resume();
+              break;
+          }
+        }),
+        subscribe(subscriber),
+      );
+
+      subscriber.add(readable).add(modeSubscription);
+
+      const onData = (data: Buffer) => {
+        subscriber.dispatch({ type: StreamEventType.Next, data });
+      };
+      readable.value.on("data", onData);
+
+      const onEnd = () => {
+        subscriber.dispatch({ type: StreamEventType.Complete });
+        readable.dispose();
+      };
+      readable.value.on("end", onEnd);
+
+      const onError = (cause: any) => {
+        subscriber.dispose({ cause });
+      };
+      readable.value.on("error", onError);
+
+      readable.value.pause();
+    }),
+  );
 
 // FIXME: Maybe remove this
 export const stringToBufferStream = (
