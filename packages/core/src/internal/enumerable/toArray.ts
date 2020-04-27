0735b4499a0680c31cc1c50e69600bc0c1c05c41
David Bordoley <bordoley@gmail.com>
2020-04-27 05:02:36 -0400

    some tweaks

--- packages/core/src/internal/enumerable/toArray.ts
+++ packages/core/src/internal/enumerable/toArray.ts
@@ -2,13 +2,14 @@ import { EnumerableLike } from "./interfaces";
 import { reduce } from "./reduce";
 import { pipe } from "../../pipe";
 
-export const toArray = <T>(enumerable: EnumerableLike<T>): readonly T[] => pipe(
-  enumerable,
-  reduce(
-    (acc, next) => {
-      acc.push(next);
-      return acc;
-    },
-    (): T[] => []
-  )
-);
+export const toArray = <T>(enumerable: EnumerableLike<T>): readonly T[] =>
+  pipe(
+    enumerable,
+    reduce(
+      (acc, next) => {
+        acc.push(next);
+        return acc;
+      },
+      (): T[] => [],
+    ),
+  );
