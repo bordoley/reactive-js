6a40f8c56970825ca44da5b2eb4e0074425a097b
David Bordoley <bordoley@gmail.com>
2020-05-10 21:24:56 -0400

    flowable tests

--- packages/core/mod/stateStore.js
+++ packages/core/mod/stateStore.js
@@ -1,9 +1,11 @@
-import { pipe } from "./functions.js";
-import { onNotify, ignoreElements, merge, onSubscribe, using, zipWithLatestFrom, } from "./observable.js";
+import { pipe, identity } from "./functions.js";
+import { onNotify, using, zipWithLatestFrom, } from "./observable.js";
 import { createActionReducer, createStreamable, } from "./streamable.js";
+import { subscribe } from "./internal/observable/subscribe.js";
 const stateStoreReducer = (state, action) => action(state);
 export const createStateStore = (initialState, equals) => createActionReducer(stateStoreReducer, initialState, equals);
-export const toStateStore = () => {
-    const createObservable = (updates) => (stream) => merge(pipe(updates, zipWithLatestFrom(stream, (updateState, prev) => updateState(prev)), onNotify(next => stream.dispatch(next)), ignoreElements(), onSubscribe(() => stream)), stream);
-    return streamable => createStreamable(updates => using(scheduler => streamable.stream(scheduler), createObservable(updates)));
-};
+export const toStateStore = () => streamable => createStreamable(updates => using(scheduler => {
+    const stream = streamable.stream(scheduler);
+    const updatesSubscription = pipe(updates, zipWithLatestFrom(stream, (updateState, prev) => updateState(prev)), onNotify(next => stream.dispatch(next)), subscribe(scheduler)).add(stream);
+    return stream.add(updatesSubscription);
+}, identity));
