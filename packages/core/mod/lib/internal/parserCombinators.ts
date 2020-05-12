893907385d47c09f02ad24b6de3c14a0532ee502
David Bordoley <bordoley@gmail.com>
2020-05-12 11:01:47 -0400

    add missing files

--- packages/core/mod/lib/internal/parserCombinators.ts
+++ packages/core/mod/lib/internal/parserCombinators.ts
@@ -1,4 +1,4 @@
-import { EnumeratorLike } from "@reactive-js/core/lib/enumerable";
+import { EnumeratorLike } from "../enumerable.ts";
 import {
   Factory,
   Operator,
@@ -7,15 +7,14 @@ import {
   pipe,
   returns,
   Predicate,
-} from "@reactive-js/core/lib/functions";
+} from "../functions.ts";
 
-// FIXME: BAD!
-import { __DEV__ } from "@reactive-js/core/lib/internal/env";
+import { __DEV__ } from "./env.ts";
 import {
   Option,
   none,
   orCompute as orComputeOption,
-} from "@reactive-js/core/lib/option";
+} from "../option.ts";
 
 export type CharCode = number;
 
