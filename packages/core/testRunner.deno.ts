9d8f1666f6ddeabe2e3cf46f7bb464012b74e74a
David Bordoley <bordoley@gmail.com>
2020-05-16 22:25:10 -0400

    add flowable tests to deno

--- packages/core/testRunner.deno.ts
+++ packages/core/testRunner.deno.ts
@@ -3,10 +3,11 @@ import { runTests } from "../../scripts/denoTestRunner.ts";
 import { tests as asyncEnumerableTests } from "./mod/test/asyncEnumerable.test.ts";
 import { tests as disposableTests } from "./mod/test/disposable.test.ts";
 import { tests as enumerableTests } from "./mod/test/enumerable.test.ts";
-//import { tests as flowableTests } from "./mod/test/flowable.test.ts";
+import { tests as flowableTests } from "./mod/test/flowable.test.ts";
+//import { tests as ioTests } from "./src/test/io.test";
+import { tests as observableTests } from "./mod/test/observable.test.ts";
 import { tests as parserCombinatorTests } from "./mod/test/internal/parserCombinators.test.ts";
 import { tests as queuesTests } from "./mod/test/internal/queues.test.ts";
-import { tests as observableTests } from "./mod/test/observable.test.ts";
 //import { tests as reactiveCacheTests } from "./reactiveCache.test";
 //import { tests as resourceManagerTests } from "./resourceManager.test";
 import { tests as stateStoreTests } from "./mod/test/stateStore.test.ts";
@@ -17,7 +18,7 @@ const tests = [
   asyncEnumerableTests,
   disposableTests,
   enumerableTests,
-  //flowableTests,
+  flowableTests,
   observableTests,
   parserCombinatorTests,
   queuesTests,
