88ef06749ebcede0bb2c11945465d5a6fe16355a
David Bordoley <bordoley@gmail.com>
2020-05-22 22:45:55 -0400

    remove the node http packages in mod

--- packages/core/mod/lib/experimental/node.js
+++
@@ -1,2 +0,0 @@
-export { createHttpRequestListener, } from "./internal/node/httpRequestListener.js";
-export { createContentEncodingDecompressTransforms, createContentEncodingCompressTransforms, } from "./internal/node/httpContentEncoding.js";
