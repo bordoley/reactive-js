54f8d411552350cbebe936e0ee15f227b671fb64
David Bordoley <bordoley@gmail.com>
2020-11-23 01:26:53 -0500

    fix unit tests

--- scripts/testRunner.deno.ts
+++ scripts/testRunner.deno.ts
@@ -1,19 +1,19 @@
-import { tests as asyncEnumerableTests } from "../mod/asyncEnumerable.test.ts";
-import { tests as disposableTests } from "../mod/disposable.test.ts";
-import { tests as enumerableTests } from "../mod/enumerable.test.ts";
-//import { tests as httpTests } from "../mod/experimental/http.test.ts";
-import { tests as parserCombinatorTests } from "../mod/experimental/parserCombinators.test.ts";
-import { tests as reactiveCacheTests } from "../mod/experimental/reactiveCache.test.ts";
-import { runTests } from "../mod/experimental/testing.deno.ts";
-import { tests as flowableTests } from "../mod/flowable.test.ts";
+import { tests as asyncEnumerableTests } from "../mod/asyncEnumerable.test.mjs";
+import { tests as disposableTests } from "../mod/disposable.test.mjs";
+import { tests as enumerableTests } from "../mod/enumerable.test.mjs";
+//import { tests as httpTests } from "../mod/experimental/http.test.mjs";
+import { tests as parserCombinatorTests } from "../mod/parserCombinators.test.mjs";
+import { tests as reactiveCacheTests } from "../mod/reactiveCache.test.mjs";
+import { runTests } from "../mod/testing.deno.mjs";
+import { tests as flowableTests } from "../mod/flowable.test.mjs";
 //import { tests as ioTests } from "./mod/io.test";
-import { tests as queuesTests } from "../mod/internal/queues.test.ts";
-import { tests as observableTests } from "../mod/observable.test.ts";
+import { tests as queuesTests } from "../mod/queues.test.mjs";
+import { tests as observableTests } from "../mod/observable.test.mjs";
 //import { tests as resourceManagerTests } from "./resourceManager.test";
-import { tests as runnableTests } from "../mod/runnable.test.ts";
-import { tests as sequenceTests } from "../mod/sequence.test.ts";
-import { tests as stateStoreTests } from "../mod/stateStore.test.ts";
-import { tests as streamableTests } from "../mod/streamable.test.ts";
+import { tests as runnableTests } from "../mod/runnable.test.mjs";
+import { tests as sequenceTests } from "../mod/sequence.test.mjs";
+import { tests as stateStoreTests } from "../mod/stateStore.test.mjs";
+import { tests as streamableTests } from "../mod/streamable.test.mjs";
 
 const tests = [
   asyncEnumerableTests,
