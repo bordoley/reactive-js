062c8dcb84dc8254d918c4feafc03323bb5a0572
David Bordoley <bordoley@gmail.com>
2020-11-23 11:36:45 -0500

    don't host cjs files

--- mod/env.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const __DEV__ = typeof process === "object" ? process.env.NODE_ENV !== "production" : false;
-const isDeno = typeof Deno === "object";
-
-exports.__DEV__ = __DEV__;
-exports.isDeno = isDeno;
