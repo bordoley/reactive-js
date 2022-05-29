9657f2a27cf1555f4d2b88729eef4e9a636596a8
David Bordoley <bordoley@gmail.com>
2022-05-29 11:21:35 -0400

    fix tests

--- scripts/testRunner.jest.ts
+++ scripts/testRunner.jest.ts
@@ -5,7 +5,6 @@
 import { runTests } from "../src/testing";
 import { tests } from "../src/__tests__";
 import { tests as nodeTests } from "../src/__tests__/node.test";
-import { tests as parserCombinatorTests } from "../src/__tests__/parserCombinators.test";
 import { tests as queuesTests } from "../src/__tests__/queues.test";
 
-runTests([...tests, nodeTests, parserCombinatorTests, queuesTests]);
+runTests([...tests, nodeTests, queuesTests]);
