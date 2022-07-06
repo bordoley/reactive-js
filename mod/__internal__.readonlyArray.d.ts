ce45d79deb99229389f217e0c58d2ec27792cc13
David Bordoley <bordoley@gmail.com>
2022-07-06 01:09:45 -0400

    rebuild, mark disposable field private

--- mod/__internal__.readonlyArray.d.ts
+++ mod/__internal__.readonlyArray.d.ts
@@ -1,15 +1,9 @@
-import { Function1, Predicate, TypePredicate, Reducer, Factory, SideEffect1 } from "./functions.mjs";
+import { Function1, Predicate, TypePredicate, SideEffect1 } from "./functions.mjs";
 declare const empty: ReadonlyArray<any>;
 declare type ReadonlyArrayOperator<TA, TB> = Function1<readonly TA[], readonly TB[]>;
 declare const everySatisfy: <T>(predicate: Predicate<T>) => Function1<readonly T[], boolean>;
-declare const fromObject: <T>() => Function1<Readonly<Record<string, T>>, readonly [
-    string,
-    T
-][]>;
-declare const join: (separator?: string) => Function1<readonly string[], string>;
 declare const keep: <T>(predicate: Predicate<T>) => ReadonlyArrayOperator<T, T>;
 declare const keepType: <TA, TB extends TA>(predicate: TypePredicate<TA, TB>) => ReadonlyArrayOperator<TA, TB>;
 declare const map: <TA, TB>(mapper: Function1<TA, TB>) => ReadonlyArrayOperator<TA, TB>;
-declare const reduce: <T, TAcc>(reducer: Reducer<T, TAcc>, initialValue: Factory<TAcc>) => Function1<readonly T[], TAcc>;
 declare const forEach: <T>(f: SideEffect1<T>) => Function1<readonly T[], readonly T[]>;
-export { ReadonlyArrayOperator, empty, everySatisfy, forEach, fromObject, join, keep, keepType, map, reduce };
+export { ReadonlyArrayOperator, empty, everySatisfy, forEach, keep, keepType, map };
