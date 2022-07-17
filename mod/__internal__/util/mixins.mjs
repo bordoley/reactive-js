bb9942bcc45e8a6720b3ff8a5364048cbf69025c
David Bordoley <bordoley@fb.com>
2022-07-17 11:11:38 -0400

    s/decorate/add

--- mod/__internal__/util/mixins.mjs
+++ mod/__internal__/util/mixins.mjs
@@ -1,19 +1,19 @@
 /// <reference types="./mixins.d.ts" />
 const { defineProperty: defineObjectProperty } = Object;
 const getPrototype = (constructor) => constructor.prototype;
-const decorateGetter = (property, get) => (Constructor) => {
+const addGetter = (property, get) => (Constructor) => {
     defineObjectProperty(getPrototype(Constructor), property, {
         get,
     });
     return Constructor;
 };
-const decorateProperty = (property, description) => (Constructor) => {
+const addProperty = (property, description) => (Constructor) => {
     defineObjectProperty(getPrototype(Constructor), property, description);
     return Constructor;
 };
-const decorateMethod = (property, f) => (Constructor) => {
+const addMethod = (property, f) => (Constructor) => {
     Constructor.prototype[property] = f;
     return Constructor;
 };
 
-export { decorateGetter, decorateMethod, decorateProperty };
+export { addGetter, addMethod, addProperty };
