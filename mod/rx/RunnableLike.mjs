b0b179db83350f2ede3be76d218266c01aae4ff4
David Bordoley <bordoley@fb.com>
2022-07-26 03:32:37 -0400

    migrate runnable lift

--- mod/rx/RunnableLike.mjs
+++ mod/rx/RunnableLike.mjs
@@ -1,2 +1,32 @@
 /// <reference types="./RunnableLike.d.ts" />
-"use strict";
+import { reactive } from '../__internal__/containers/StatefulContainerLikeInternal.mjs';
+import { pipe, pipeUnsafe, newInstance } from '../functions.mjs';
+import { ReactiveContainerLike_sinkInto } from '../rx.mjs';
+import '../util/DisposableLike.mjs';
+import { sourceFrom } from './ReactiveContainerLike.mjs';
+import { dispose } from '../__internal__/util/DisposableLikeInternal.mjs';
+
+const lift = /*@__PURE__*/ (() => {
+    class LiftedRunnable {
+        constructor(src, operators) {
+            this.src = src;
+            this.operators = operators;
+        }
+        [ReactiveContainerLike_sinkInto](sink) {
+            pipe(pipeUnsafe(sink, ...this.operators), sourceFrom(this.src), dispose());
+        }
+    }
+    return (operator) => (runnable) => {
+        const src = runnable instanceof LiftedRunnable ? runnable.src : runnable;
+        const allFunctions = runnable instanceof LiftedRunnable
+            ? [operator, ...runnable.operators]
+            : [operator];
+        return newInstance(LiftedRunnable, src, allFunctions);
+    };
+})();
+const liftT = {
+    lift,
+    variance: reactive,
+};
+
+export { liftT };
