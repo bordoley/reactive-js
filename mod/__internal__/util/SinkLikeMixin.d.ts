6c9dcb0c6fda7b6f008d1e5481255b2d62d80f33
David Bordoley <bordoley@fb.com>
2022-07-28 01:17:06 -0400

    use symbols for private variables in sinks

--- mod/__internal__/util/SinkLikeMixin.d.ts
+++ mod/__internal__/util/SinkLikeMixin.d.ts
@@ -2,52 +2,25 @@ import { Predicate, Function1, SideEffect1, Reducer, Factory } from "../../funct
 import { SinkLike, SinkLike_notify } from "../../util.mjs";
 import { DisposableLike } from "./DisposableLikeInternal.mjs";
 import { Object_properties, Object_init } from "./Object.mjs";
-declare const Sink_delegate: unique symbol;
 declare const createSink: <T>() => SinkLike<T>;
 declare const keepSinkMixin: <T>() => DisposableLike & {
-    [Object_properties]: {
-        [Sink_delegate]: SinkLike<T>;
-        predicate: Predicate<T>;
-    };
-    [Object_init](this: {
-        [Sink_delegate]: SinkLike<T>;
-        predicate: Predicate<T>;
-    }, delegate: SinkLike<T>, predicate: Predicate<T>): void;
+    [Object_properties]: unknown;
+    [Object_init](this: unknown, delegate: SinkLike<T>, predicate: Predicate<T>): void;
     [SinkLike_notify](next: T): void;
 };
 declare const mapSinkMixin: <TA, TB>() => DisposableLike & {
-    [Object_properties]: {
-        [Sink_delegate]: SinkLike<TB>;
-        mapper: Function1<TA, TB>;
-    };
-    [Object_init](this: {
-        [Sink_delegate]: SinkLike<TB>;
-        mapper: Function1<TA, TB>;
-    }, delegate: SinkLike<TB>, mapper: Function1<TA, TB>): void;
+    [Object_properties]: unknown;
+    [Object_init](this: unknown, delegate: SinkLike<TB>, mapper: Function1<TA, TB>): void;
     [SinkLike_notify](next: TA): void;
 };
 declare const onNotifySinkMixin: <T>() => DisposableLike & {
-    [Object_properties]: {
-        [Sink_delegate]: SinkLike<T>;
-        onNotify: SideEffect1<T>;
-    };
-    [Object_init](this: {
-        [Sink_delegate]: SinkLike<T>;
-        onNotify: SideEffect1<T>;
-    }, delegate: SinkLike<T>, monNotify: SideEffect1<T>): void;
+    [Object_properties]: unknown;
+    [Object_init](this: unknown, delegate: SinkLike<T>, monNotify: SideEffect1<T>): void;
     [SinkLike_notify](next: T): void;
 };
 declare const scanSinkMixin: <T, TAcc>() => DisposableLike & {
-    [Object_properties]: {
-        [Sink_delegate]: SinkLike<TAcc>;
-        reducer: Reducer<T, TAcc>;
-        acc: TAcc;
-    };
-    [Object_init](this: {
-        [Sink_delegate]: SinkLike<TAcc>;
-        reducer: Reducer<T, TAcc>;
-        acc: TAcc;
-    }, delegate: SinkLike<TAcc>, reducer: Reducer<T, TAcc>, initialValue: Factory<TAcc>): void;
+    [Object_properties]: unknown;
+    [Object_init](this: unknown, delegate: SinkLike<TAcc>, reducer: Reducer<T, TAcc>, initialValue: Factory<TAcc>): void;
     [SinkLike_notify](next: T): void;
 };
-export { Sink_delegate, createSink, keepSinkMixin, mapSinkMixin, onNotifySinkMixin, scanSinkMixin };
+export { createSink, keepSinkMixin, mapSinkMixin, onNotifySinkMixin, scanSinkMixin };
--- mod/__internal__/util/SinkLikeMixin.mjs
+++ mod/__internal__/util/SinkLikeMixin.mjs
@@ -6,7 +6,7 @@ import { disposableMixin, delegatingDisposableMixin } from './DisposableLikeMixi
 import { dispose } from './DisposableLikeInternal.mjs';
 import { Object_properties, Object_init, init, mixWith, createObjectFactory } from './Object.mjs';
 
-const Sink_delegate = Symbol("Sink_delegate");
+const Sink_private_delegate = Symbol("Sink_private_delegate");
 const createSink = /*@__PURE__*/ (() => pipe({
     [Object_properties]: {},
     [Object_init]() {
@@ -15,82 +15,87 @@ const createSink = /*@__PURE__*/ (() => pipe({
     [SinkLike_notify](_) { },
 }, mixWith(disposableMixin), createObjectFactory()))();
 const keepSinkMixin = /*@__PURE__*/ (() => {
+    const KeepSink_private_predicate = Symbol("KeepSink_private_predicate");
     return pipe({
         [Object_properties]: {
-            [Sink_delegate]: none,
-            predicate: none,
+            [Sink_private_delegate]: none,
+            [KeepSink_private_predicate]: none,
         },
         [Object_init](delegate, predicate) {
             init(delegatingDisposableMixin, this, delegate);
-            this[Sink_delegate] = delegate;
-            this.predicate = predicate;
+            this[Sink_private_delegate] = delegate;
+            this[KeepSink_private_predicate] = predicate;
         },
         [SinkLike_notify](next) {
-            if (this.predicate(next)) {
-                pipe(this[Sink_delegate], notify(next));
+            if (this[KeepSink_private_predicate](next)) {
+                pipe(this[Sink_private_delegate], notify(next));
             }
         },
     }, mixWith(delegatingDisposableMixin), returns);
 })();
 const mapSinkMixin = /*@__PURE__*/ (() => {
+    const MapSink_private_mapper = Symbol("MapSink_private_mapper");
     return pipe({
         [Object_properties]: {
-            [Sink_delegate]: none,
+            [Sink_private_delegate]: none,
             mapper: none,
         },
         [Object_init](delegate, mapper) {
             init(delegatingDisposableMixin, this, delegate);
-            this[Sink_delegate] = delegate;
-            this.mapper = mapper;
+            this[Sink_private_delegate] = delegate;
+            this[MapSink_private_mapper] = mapper;
         },
         [SinkLike_notify](next) {
-            const mapped = this.mapper(next);
-            pipe(this[Sink_delegate], notify(mapped));
+            const mapped = this[MapSink_private_mapper](next);
+            pipe(this[Sink_private_delegate], notify(mapped));
         },
     }, mixWith(delegatingDisposableMixin), returns);
 })();
 const onNotifySinkMixin = /*@__PURE__*/ (() => {
+    const OnNotifySink_private_onNotify = Symbol("OnNotifySink_private_onNotify");
     return pipe({
         [Object_properties]: {
-            [Sink_delegate]: none,
-            onNotify: none,
+            [Sink_private_delegate]: none,
+            [OnNotifySink_private_onNotify]: none,
         },
         [Object_init](delegate, onNotify) {
             init(delegatingDisposableMixin, this, delegate);
-            this[Sink_delegate] = delegate;
-            this.onNotify = onNotify;
+            this[Sink_private_delegate] = delegate;
+            this[OnNotifySink_private_onNotify] = onNotify;
         },
         [SinkLike_notify](next) {
-            this.onNotify(next);
-            pipe(this[Sink_delegate], notify(next));
+            this[OnNotifySink_private_onNotify](next);
+            pipe(this[Sink_private_delegate], notify(next));
         },
     }, mixWith(delegatingDisposableMixin), returns);
 })();
 const scanSinkMixin = /*@__PURE__*/ (() => {
+    const ScanSink_private_reducer = Symbol("ScanSink_private_reducer");
+    const ScanSink_private_acc = Symbol("ScanSink_private_acc");
     return pipe({
         [Object_properties]: {
-            [Sink_delegate]: none,
-            reducer: none,
-            acc: none,
+            [Sink_private_delegate]: none,
+            [ScanSink_private_reducer]: none,
+            [ScanSink_private_acc]: none,
         },
         [Object_init](delegate, reducer, initialValue) {
             init(delegatingDisposableMixin, this, delegate);
-            this[Sink_delegate] = delegate;
-            this.reducer = reducer;
+            this[Sink_private_delegate] = delegate;
+            this[ScanSink_private_reducer] = reducer;
             try {
                 const acc = initialValue();
-                this.acc = acc;
+                this[ScanSink_private_acc] = acc;
             }
             catch (cause) {
                 pipe(this, dispose({ cause }));
             }
         },
         [SinkLike_notify](next) {
-            const nextAcc = this.reducer(this.acc, next);
-            this.acc = nextAcc;
-            pipe(this[Sink_delegate], notify(nextAcc));
+            const nextAcc = this[ScanSink_private_reducer](this[ScanSink_private_acc], next);
+            this[ScanSink_private_acc] = nextAcc;
+            pipe(this[Sink_private_delegate], notify(nextAcc));
         },
     }, mixWith(delegatingDisposableMixin), returns);
 })();
 
-export { Sink_delegate, createSink, keepSinkMixin, mapSinkMixin, onNotifySinkMixin, scanSinkMixin };
+export { createSink, keepSinkMixin, mapSinkMixin, onNotifySinkMixin, scanSinkMixin };
