54c2f467237bee29cb8f31cc11cc364d8327df91
David Bordoley <bordoley@gmail.com>
2022-08-10 18:09:30 -0400

    add createAsyncEnumerator

--- mod/streaming.mjs
+++ mod/streaming.mjs
@@ -1,58 +1,77 @@
 /// <reference types="./streaming.d.ts" />
 import { multicast, scan, mergeT, distinctUntilChanged } from './__internal__/rx/__internal__ObservableLike.mjs';
 import { delegatingDisposableMixin } from './__internal__/util/__internal__Disposables.mjs';
-import { createInstanceFactory, clazz, __extends, init } from './__internal__/util/__internal__Objects.mjs';
+import { clazz, __extends, init, createInstanceFactory } from './__internal__/util/__internal__Objects.mjs';
 import { concatWith } from './containers/ContainerLike.mjs';
 import { toObservable } from './containers/ReadonlyArrayLike.mjs';
-import { unsafeCast, pipe, none, newInstance, getLength, composeUnsafe, returns, updateReducer } from './functions.mjs';
+import { pipe, unsafeCast, none, returns, newInstance, getLength, composeUnsafe, updateReducer } from './functions.mjs';
 import { createSubject, MulticastObservableLike_observerCount, MulticastObservableLike_replay, ObservableLike_isEnumerable, ObservableLike_isRunnable, ReactiveContainerLike_sinkInto, createObservable } from './rx.mjs';
 import { getObserverCount, getReplay } from './rx/MulticastObservableLike.mjs';
 import { sinkInto } from './rx/ReactiveContainerLike.mjs';
 import { publish } from './rx/SubjectLike.mjs';
 import { DispatcherLike_scheduler, DispatcherLike_dispatch } from './scheduling.mjs';
 import { dispatch } from './scheduling/DispatcherLike.mjs';
+import { SourceLike_move } from './util.mjs';
 import { add } from './util/DisposableLike.mjs';
 
 /** @ignore */
 const StreamableLike_stream = Symbol("StreamableLike_stream");
+const streamMixin = (() => {
+    return pipe(clazz(__extends(delegatingDisposableMixin), function Stream(instance, op, scheduler, replay) {
+        const subject = createSubject({ replay });
+        init(delegatingDisposableMixin, instance, subject);
+        unsafeCast(instance);
+        instance[DispatcherLike_scheduler] = scheduler;
+        instance.subject = subject;
+        instance.observable = pipe(subject, op, multicast(scheduler, { replay }), add(instance));
+        return instance;
+    }, {
+        subject: none,
+        observable: none,
+        [DispatcherLike_scheduler]: none,
+    }, {
+        get [MulticastObservableLike_observerCount]() {
+            unsafeCast(this);
+            return getObserverCount(this.observable);
+        },
+        get [MulticastObservableLike_replay]() {
+            unsafeCast(this);
+            return getReplay(this.observable);
+        },
+        [ObservableLike_isEnumerable]: false,
+        [ObservableLike_isRunnable]: false,
+        [DispatcherLike_dispatch](req) {
+            unsafeCast(this);
+            pipe(this.subject, publish(req));
+        },
+        [ReactiveContainerLike_sinkInto](observer) {
+            unsafeCast(this);
+            pipe(this.observable, sinkInto(observer));
+        },
+    }), returns);
+})();
 const createStream = /*@__PURE__*/ (() => {
-    const createStreamInternal = (() => {
-        return createInstanceFactory(clazz(__extends(delegatingDisposableMixin), function StreamImpl(instance, op, scheduler, replay) {
-            const subject = createSubject({ replay });
-            init(delegatingDisposableMixin, instance, subject);
-            unsafeCast(instance);
-            instance[DispatcherLike_scheduler] = scheduler;
-            instance.subject = subject;
-            instance.observable = pipe(subject, op, multicast(scheduler, { replay }), add(instance));
+    const createStreamInternal = createInstanceFactory(streamMixin());
+    return (op, scheduler, options) => {
+        const { replay = 0 } = options !== null && options !== void 0 ? options : {};
+        return createStreamInternal(op, scheduler, replay);
+    };
+})();
+const createAsyncEnumerator = /*@__PURE__*/ (() => {
+    const createAsyncEnumeratorInternal = (() => {
+        const typedStreamMixin = streamMixin();
+        return createInstanceFactory(clazz(__extends(typedStreamMixin), function AsyncEnumerator(instance, op, scheduler, replay) {
+            init(typedStreamMixin, instance, op, scheduler, replay);
             return instance;
-        }, {
-            subject: none,
-            observable: none,
-            [DispatcherLike_scheduler]: none,
-        }, {
-            get [MulticastObservableLike_observerCount]() {
-                unsafeCast(this);
-                return getObserverCount(this.observable);
-            },
-            get [MulticastObservableLike_replay]() {
-                unsafeCast(this);
-                return getReplay(this.observable);
-            },
-            [ObservableLike_isEnumerable]: false,
-            [ObservableLike_isRunnable]: false,
-            [DispatcherLike_dispatch](req) {
-                unsafeCast(this);
-                pipe(this.subject, publish(req));
-            },
-            [ReactiveContainerLike_sinkInto](observer) {
-                unsafeCast(this);
-                pipe(this.observable, sinkInto(observer));
+        }, {}, {
+            [SourceLike_move]() {
+                pipe(this, dispatch(none));
             },
         }));
     })();
     return (op, scheduler, options) => {
         const { replay = 0 } = options !== null && options !== void 0 ? options : {};
-        return createStreamInternal(op, scheduler, replay);
+        return createAsyncEnumeratorInternal(op, scheduler, replay);
     };
 })();
 const createStreamble = /*@__PURE__*/ (() => {
@@ -105,4 +124,4 @@ const createActionReducer = (reducer, initialState, options) => createLiftedStre
  */
 const createStateStore = (initialState, options) => createActionReducer(updateReducer, initialState, options);
 
-export { StreamableLike_stream, createActionReducer, createLiftedFlowable, createLiftedStreamable, createStateStore, createStream, createStreamble };
+export { StreamableLike_stream, createActionReducer, createAsyncEnumerator, createLiftedFlowable, createLiftedStreamable, createStateStore, createStream, createStreamble };
