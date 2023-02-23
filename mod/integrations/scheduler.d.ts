c18513049cdc1eb7201185c6566a4ae4e2b24889
David Bordoley <bordoley@gmail.com>
2023-02-23 11:26:07 -0500

    exclude react/scheduler from mod build

--- mod/integrations/scheduler.d.ts
+++
@@ -1,7 +0,0 @@
-import { Factory } from "../functions.js";
-import { SchedulerLike } from "../scheduling.js";
-export declare const createSchedulerWithIdlePriority: Factory<SchedulerLike>;
-export declare const createSchedulerWithImmediatePriority: Factory<SchedulerLike>;
-export declare const createSchedulerWithNormalPriority: Factory<SchedulerLike>;
-export declare const createSchedulerWithLowPriority: Factory<SchedulerLike>;
-export declare const createSchedulerWithUserBlockingPriority: Factory<SchedulerLike>;
