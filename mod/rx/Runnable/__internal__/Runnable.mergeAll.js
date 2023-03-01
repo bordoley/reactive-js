cf4cac45a5e436e5c7493e3f5ad4034c4a5deac9
David Bordoley <bordoley@gmail.com>
2023-03-01 13:48:57 -0800

    remove cast

--- mod/rx/Runnable/__internal__/Runnable.mergeAll.js
+++ mod/rx/Runnable/__internal__/Runnable.mergeAll.js
@@ -2,5 +2,6 @@
 
 import HigherOrderObservable_mergeAll from "../../HigherOrderObservable/__internal__/HigherOrderObservable.mergeAll.js";
 import Runnable_lift from "./Runnable.lift.js";
-const Runnable_mergeAll = /*@__PURE__*/ HigherOrderObservable_mergeAll(Runnable_lift);
+const Runnable_mergeAll = 
+/*@__PURE__*/ HigherOrderObservable_mergeAll(Runnable_lift);
 export default Runnable_mergeAll;
