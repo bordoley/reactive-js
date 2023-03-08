9d0efb51c8b1e79920efb1bb8ed7931bbd5dfd44
David Bordoley <bordoley@gmail.com>
2023-03-08 08:39:33 -0500

    remove unused module

--- mod/rx/Observer/__internal__/Observer.decorateNotifyForDev.d.ts
+++
@@ -1,8 +0,0 @@
-import { MixinPrototype } from "../../../__internal__/mixins.js";
-import { ObserverLike, ObserverLike_notify } from "../../../rx.js";
-declare const Observer_decorateNotifyForDev: <TThis extends ObserverLike<unknown>, TNext>(mixin: MixinPrototype<{
-    [ObserverLike_notify](this: TThis, next: TNext): void;
-}>) => {
-    [ObserverLike_notify](this: TThis, next: TNext): void;
-} | {};
-export default Observer_decorateNotifyForDev;
