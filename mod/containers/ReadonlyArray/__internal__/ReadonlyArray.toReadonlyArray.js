072a7716c6faaabdd5ba825f9af794d7fcded89d
David Bordoley <bordoley@gmail.com>
2023-03-08 08:43:36 -0500

    avoid allocating a new array

--- mod/containers/ReadonlyArray/__internal__/ReadonlyArray.toReadonlyArray.js
+++ mod/containers/ReadonlyArray/__internal__/ReadonlyArray.toReadonlyArray.js
@@ -2,7 +2,9 @@
 
 import ReadonlyArray_toContainer from "./ReadonlyArray.toContainer.js";
 const ReadonlyArray_toReadonlyArray = 
-/*@__PURE__*/ ReadonlyArray_toContainer((values, startIndex, count) => count >= 0
-    ? values.slice(startIndex, count + startIndex)
-    : values.slice(startIndex + count + 1, startIndex + 1).reverse());
+/*@__PURE__*/ ReadonlyArray_toContainer((values, startIndex, count) => startIndex === 0 && count === values.length
+    ? values
+    : count >= 0
+        ? values.slice(startIndex, count + startIndex)
+        : values.slice(startIndex + count + 1, startIndex + 1).reverse());
 export default ReadonlyArray_toReadonlyArray;
--- src/containers/ReadonlyArray/__internal__/ReadonlyArray.toReadonlyArray.ts
+++ src/containers/ReadonlyArray/__internal__/ReadonlyArray.toReadonlyArray.ts
@@ -10,7 +10,9 @@ const ReadonlyArray_toReadonlyArray: ToReadonlyArray<
 >["toReadonlyArray"] =
   /*@__PURE__*/ ReadonlyArray_toContainer<ReadonlyArrayLike>(
     <T>(values: readonly T[], startIndex: number, count: number) =>
-      count >= 0
+      startIndex === 0 && count === values.length
+        ? values
+        : count >= 0
         ? values.slice(startIndex, count + startIndex)
         : values.slice(startIndex + count + 1, startIndex + 1).reverse(),
   );
