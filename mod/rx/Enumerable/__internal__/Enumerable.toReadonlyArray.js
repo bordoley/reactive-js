6cade162b9fa9a58e0314de9bd2d6e8618f373d1
David Bordoley <bordoley@gmail.com>
2023-03-09 10:07:21 -0500

    fix #342

--- mod/rx/Enumerable/__internal__/Enumerable.toReadonlyArray.js
+++ mod/rx/Enumerable/__internal__/Enumerable.toReadonlyArray.js
@@ -1,7 +1,17 @@
 /// <reference types="./Enumerable.toReadonlyArray.d.ts" />
 
 import Enumerator_toReadonlyArray from "../../../containers/Enumerator/__internal__/Enumerator.toArray.js";
-import { pipe } from "../../../functions.js";
+import { isSome, pipe, raiseError } from "../../../functions.js";
+import { DisposableLike_dispose, DisposableLike_error } from "../../../util.js";
 import Enumerable_enumerate from "./Enumerable.enumerate.js";
-const Enumerable_toReadonlyArray = () => (enumerable) => pipe(enumerable, Enumerable_enumerate(), Enumerator_toReadonlyArray());
+const Enumerable_toReadonlyArray = () => (enumerable) => {
+    const enumerator = pipe(enumerable, Enumerable_enumerate());
+    const result = pipe(enumerator, Enumerator_toReadonlyArray());
+    enumerator[DisposableLike_dispose]();
+    const err = enumerator[DisposableLike_error];
+    if (isSome(err)) {
+        raiseError(err);
+    }
+    return result;
+};
 export default Enumerable_toReadonlyArray;
--- src/rx/Enumerable/__internal__/Enumerable.toReadonlyArray.ts
+++ src/rx/Enumerable/__internal__/Enumerable.toReadonlyArray.ts
@@ -1,13 +1,25 @@
 import { ToReadonlyArray } from "../../../containers.js";
 import Enumerator_toReadonlyArray from "../../../containers/Enumerator/__internal__/Enumerator.toArray.js";
-import { pipe } from "../../../functions.js";
+import { isSome, pipe, raiseError } from "../../../functions.js";
 import { EnumerableLike } from "../../../rx.js";
+import { DisposableLike_dispose, DisposableLike_error } from "../../../util.js";
 import Enumerable_enumerate from "./Enumerable.enumerate.js";
 
 const Enumerable_toReadonlyArray: ToReadonlyArray<EnumerableLike>["toReadonlyArray"] =
 
     <T>() =>
-    (enumerable: EnumerableLike<T>) =>
-      pipe(enumerable, Enumerable_enumerate<T>(), Enumerator_toReadonlyArray());
+    (enumerable: EnumerableLike<T>) => {
+      const enumerator = pipe(enumerable, Enumerable_enumerate<T>());
+      const result = pipe(enumerator, Enumerator_toReadonlyArray());
+
+      enumerator[DisposableLike_dispose]();
+
+      const err = enumerator[DisposableLike_error];
+      if (isSome(err)) {
+        raiseError(err);
+      }
+
+      return result;
+    };
 
 export default Enumerable_toReadonlyArray;
