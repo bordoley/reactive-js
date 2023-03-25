27c90cfcd3aa347f5f99170b288d7c910e805c51
David Bordoley <bordoley@gmail.com>
2023-03-25 08:45:38 -0400

    start relay integration

---
+++ src/integrations/relay.ts
@@ -0,0 +1,41 @@
+import {
+  CacheConfig,
+  Environment,
+  fetchQuery as relayFetchQuery,
+  FetchQueryFetchPolicy,
+  GraphQLTaggedNode,
+  OperationType,
+} from "relay-runtime";
+import { bind, pipe } from "../functions.js";
+import { DispatcherLike_complete, ObservableLike } from "../rx.js";
+import * as Observable from "../rx/Observable.js";
+import { QueueableLike_push } from "../util.js";
+import * as Disposable from "../util/Disposable.js";
+
+export const fetchQuery = <T extends OperationType>(
+  environment: Environment,
+  taggedNode: GraphQLTaggedNode,
+  variables: T["variables"],
+  cacheConfig?: {
+    networkCacheConfig?: CacheConfig;
+    fetchPolicy?: FetchQueryFetchPolicy;
+  },
+): ObservableLike<T["response"]> =>
+  Observable.create(observer => {
+    const relaySubscription = relayFetchQuery(
+      environment,
+      taggedNode,
+      variables,
+      cacheConfig,
+    ).subscribe({
+      start: () => {},
+      complete: bind(observer[DispatcherLike_complete], observer),
+      error: Disposable.toErrorHandler(observer),
+      next: bind(observer[QueueableLike_push], observer),
+    });
+
+    pipe(
+      observer,
+      Disposable.onDisposed(relaySubscription.unsubscribe, relaySubscription),
+    );
+  });
