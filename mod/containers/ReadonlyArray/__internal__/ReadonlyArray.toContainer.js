33843d5b69a524d8457fe7905831066e1919d052
David Bordoley <bordoley@gmail.com>
2023-04-01 07:09:07 -0400

    tweak ReadonlyArray_toContainer index parsing

--- mod/containers/ReadonlyArray/__internal__/ReadonlyArray.toContainer.js
+++ mod/containers/ReadonlyArray/__internal__/ReadonlyArray.toContainer.js
@@ -1,28 +1,17 @@
 /// <reference types="./ReadonlyArray.toContainer.d.ts" />
 
+import { MAX_SAFE_INTEGER } from "../../../__internal__/constants.js";
 import { abs, clamp, min } from "../../../__internal__/math.js";
-import { isSome } from "../../../functions.js";
 import ReadonlyArray_getLength from "./ReadonlyArray.getLength.js";
 const ReadonlyArray_toContainer = (factory) => (options) => values => {
     const valuesLength = ReadonlyArray_getLength(values);
-    const { start: startOption, count: countOption, ...tail } = options ?? {};
-    const { start, count } = (() => {
-        if (isSome(countOption) && countOption >= 0) {
-            const start = clamp(0, startOption ?? 0, valuesLength);
-            const count = clamp(0, countOption, valuesLength - start);
-            return { start, count };
-        }
-        else if (isSome(countOption) && countOption < 0) {
-            const start = clamp(-1, startOption ?? valuesLength - 1, valuesLength - 1);
-            const count = -min(abs(countOption), start + 1);
-            return { start, count };
-        }
-        else {
-            const start = clamp(0, startOption ?? 0, valuesLength);
-            const count = valuesLength - start;
-            return { start, count };
-        }
-    })();
+    const { start: startOption, count: countOption = MAX_SAFE_INTEGER, ...tail } = options ?? {};
+    const start = countOption >= 0
+        ? clamp(0, startOption ?? 0, valuesLength)
+        : clamp(-1, startOption ?? valuesLength - 1, valuesLength - 1);
+    const count = countOption >= 0
+        ? clamp(0, countOption, valuesLength - start)
+        : -min(abs(countOption), start + 1);
     return factory(values, start, count, tail);
 };
 export default ReadonlyArray_toContainer;
--- src/containers/ReadonlyArray/__internal__/ReadonlyArray.toContainer.ts
+++ src/containers/ReadonlyArray/__internal__/ReadonlyArray.toContainer.ts
@@ -1,10 +1,11 @@
+import { MAX_SAFE_INTEGER } from "../../../__internal__/constants.js";
 import { abs, clamp, min } from "../../../__internal__/math.js";
 import {
   ContainerLike,
   ContainerOf,
   ReadonlyArrayLike,
 } from "../../../containers.js";
-import { Function1, isSome } from "../../../functions.js";
+import { Function1 } from "../../../functions.js";
 import ReadonlyArray_getLength from "./ReadonlyArray.getLength.js";
 
 const ReadonlyArray_toContainer =
@@ -24,30 +25,21 @@ const ReadonlyArray_toContainer =
   ): Function1<ReadonlyArrayLike<T>, ContainerOf<C, T>> =>
   values => {
     const valuesLength = ReadonlyArray_getLength(values);
-    const { start: startOption, count: countOption, ...tail } = options ?? {};
+    const {
+      start: startOption,
+      count: countOption = MAX_SAFE_INTEGER,
+      ...tail
+    } = options ?? {};
+    
+    const start =
+      countOption >= 0
+        ? clamp(0, startOption ?? 0, valuesLength)
+        : clamp(-1, startOption ?? valuesLength - 1, valuesLength - 1);
 
-    const { start, count } = (() => {
-      if (isSome(countOption) && countOption >= 0) {
-        const start = clamp(0, startOption ?? 0, valuesLength);
-        const count = clamp(0, countOption, valuesLength - start);
-
-        return { start, count };
-      } else if (isSome(countOption) && countOption < 0) {
-        const start = clamp(
-          -1,
-          startOption ?? valuesLength - 1,
-          valuesLength - 1,
-        );
-        const count = -min(abs(countOption), start + 1);
-
-        return { start, count };
-      } else {
-        const start = clamp(0, startOption ?? 0, valuesLength);
-        const count = valuesLength - start;
-
-        return { start, count };
-      }
-    })();
+    const count =
+      countOption >= 0
+        ? clamp(0, countOption, valuesLength - start)
+        : -min(abs(countOption), start + 1);
 
     return factory(values, start, count, tail as O);
   };
