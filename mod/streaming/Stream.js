190773434bfec521462074bbccb38279479feb8f
David Bordoley <bordoley@gmail.com>
2023-04-26 11:43:03 -0400

    remove Stream.sinkInto as nothing uses it anymore

--- mod/streaming/Stream.js
+++ mod/streaming/Stream.js
@@ -1,6 +1,4 @@
 /// <reference types="./Stream.d.ts" />
 
-import Stream_sinkInto from "./Stream/__internal__/Stream.sinkInto.js";
 import Stream_syncState from "./Stream/__internal__/Stream.syncState.js";
-export const sinkInto = Stream_sinkInto;
 export const syncState = Stream_syncState;
--- src/streaming/Stream.ts
+++ src/streaming/Stream.ts
@@ -1,5 +1,3 @@
-import Stream_sinkInto from "./Stream/__internal__/Stream.sinkInto.js";
 import Stream_syncState from "./Stream/__internal__/Stream.syncState.js";
 
-export const sinkInto = Stream_sinkInto;
 export const syncState = Stream_syncState;
