3b73c11760ce5d73968bc8f9e230df79b7c1b41a
David Bordoley <bordoley@gmail.com>
2023-05-05 10:35:46 -0400

    init store observable with the value when its subscribed to

--- mod/util/Store/__internal__/Store.toObservable.js
+++ mod/util/Store/__internal__/Store.toObservable.js
@@ -1,8 +1,9 @@
 /// <reference types="./Store.toObservable.d.ts" />
 
 import { pipe } from "../../../functions.js";
-import Observable_startWith from "../../../rx/Observable/__internal__/Observable.startWith.js";
+import Observable_concatWith from "../../../rx/Observable/__internal__/Observable.concatWith.js";
+import Observable_fromFactory from "../../../rx/Observable/__internal__/Observable.fromFactory.js";
 import { StoreLike_value } from "../../../util.js";
 import EventSource_toObservable from "../../EventSource/__internal__/EventSource.toObservable.js";
-const Store_toObservable = () => (store) => pipe(store, EventSource_toObservable(), Observable_startWith(store[StoreLike_value]));
+const Store_toObservable = () => (store) => pipe(Observable_fromFactory(() => store[StoreLike_value]), Observable_concatWith(pipe(store, EventSource_toObservable())));
 export default Store_toObservable;
--- src/util/Store/__internal__/Store.toObservable.ts
+++ src/util/Store/__internal__/Store.toObservable.ts
@@ -1,6 +1,6 @@
 import { pipe } from "../../../functions.js";
-import { ObservableContainer } from "../../../rx.js";
-import Observable_startWith from "../../../rx/Observable/__internal__/Observable.startWith.js";
+import Observable_concatWith from "../../../rx/Observable/__internal__/Observable.concatWith.js";
+import Observable_fromFactory from "../../../rx/Observable/__internal__/Observable.fromFactory.js";
 import { StoreLike, StoreLike_value } from "../../../util.js";
 import EventSource_toObservable from "../../EventSource/__internal__/EventSource.toObservable.js";
 
@@ -8,9 +8,8 @@ const Store_toObservable =
   <T>() =>
   (store: StoreLike<T>) =>
     pipe(
-      store,
-      EventSource_toObservable(),
-      Observable_startWith<ObservableContainer, T>(store[StoreLike_value]),
+      Observable_fromFactory(() => store[StoreLike_value]),
+      Observable_concatWith(pipe(store, EventSource_toObservable())),
     );
 
 export default Store_toObservable;
