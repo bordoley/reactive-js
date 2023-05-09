8162d01122e72874ea59ea7148007bc297c555d4
David Bordoley <bordoley@gmail.com>
2023-05-09 05:22:53 -0400

    remove dead file

--- mod/__internal__/scheduling.js
+++
@@ -1,4 +0,0 @@
-/// <reference types="./scheduling.d.ts" />
-
-import { __ContinuationLike_activeChild as ContinuationLike_activeChild, __ContinuationLike_parent as ContinuationLike_parent, __ContinuationLike_run as ContinuationLike_run, __ContinuationLike_scheduler as ContinuationLike_scheduler, __ContinuationSchedulerLike_schedule as ContinuationSchedulerLike_schedule, __SchedulerTaskLike_continuation as SchedulerTaskLike_continuation, __SchedulerTaskLike_dueTime as SchedulerTaskLike_dueTime, __SchedulerTaskLike_id as SchedulerTaskLike_id, } from "./symbols.js";
-export { ContinuationLike_activeChild, ContinuationLike_parent, ContinuationLike_run, ContinuationLike_scheduler, ContinuationSchedulerLike_schedule, SchedulerTaskLike_continuation, SchedulerTaskLike_dueTime, SchedulerTaskLike_id, };
