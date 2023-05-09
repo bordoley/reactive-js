763d9f040b18009b445272215b8195b9f0dd037a
David Bordoley <bordoley@gmail.com>
2023-05-09 19:29:29 -0400

    remove unused file

--- mod/DeferredObservable/__internal__/DeferredObservable.throttle.js
+++ mod/DeferredObservable/__internal__/DeferredObservable.throttle.js
@@ -2,7 +2,7 @@
 
 import HigherOrderObservable_throttle from "../../HigherOrderObservable/__internal__/HigherOrderObservable.throttle.js";
 import ReadonlyArray_toObservable from "../../ReadonlyArray/__internal__/ReadonlyArray.toObservable.js";
-import DeferredObservable_lift from "./DeferredObservable..lift.js";
+import DeferredObservable_lift from "./DeferredObservable.lift.js";
 const DeferredObservable_throttle = 
 /*@__PURE__*/ (() => HigherOrderObservable_throttle(ReadonlyArray_toObservable, DeferredObservable_lift))();
 export default DeferredObservable_throttle;
