3effe2d10cf521443e3ea5c66c557cf0dc0fa515
David Bordoley <bordoley@gmail.com>
2023-05-15 19:45:27 -0400

    another

--- mod/AsyncIterable.js
+++ mod/AsyncIterable.js
@@ -1,6 +1,6 @@
 /// <reference types="./AsyncIterable.d.ts" />
 
 import AsyncIterable_flow from "./AsyncIterable/__internal__/AsyncIterable.flow.js";
-import AsyncIterable_toDeferredObservable from "./AsyncIterable/__internal__/AsyncIterable.toDeferredObservable.js";
+import AsyncIterable_toObservable from "./AsyncIterable/__internal__/AsyncIterable.toObservable.js";
 export const flow = AsyncIterable_flow;
-export const toDeferredObservable = AsyncIterable_toDeferredObservable;
+export const toObservable = AsyncIterable_toObservable;
---
+++ mod/AsyncIterable/__internal__/AsyncIterable.toObservable.d.ts
@@ -0,0 +1,3 @@
+import type * as AsyncIterable from "../../AsyncIterable.js";
+declare const AsyncIterable_toObservable: AsyncIterable.Signature["toObservable"];
+export default AsyncIterable_toObservable;
