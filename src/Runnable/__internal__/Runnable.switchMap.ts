733edfb36492a5344838f27e24fb5eb352a0b06b
David Bordoley <bordoley@gmail.com>
2023-05-16 14:02:20 -0400

    add tests

--- src/Runnable/__internal__/Runnable.switchMap.ts
+++ src/Runnable/__internal__/Runnable.switchMap.ts
@@ -2,7 +2,7 @@ import Observable_map from "../../Observable/__internal__/Observable.map.js";
 import type * as Runnable from "../../Runnable.js";
 import { Function1, compose } from "../../functions.js";
 import { RunnableLike } from "../../types.js";
-import Runnable_exhaust from "./Runnable.exhaust.js";
+import Runnable_switchAll from "./Runnable.switchAll.js";
 
 const Runnable_switchMap: Runnable.Signature["switchMap"] = <TA, TB>(
   selector: Function1<TA, RunnableLike<TB>>,
@@ -12,7 +12,7 @@ const Runnable_switchMap: Runnable.Signature["switchMap"] = <TA, TB>(
       RunnableLike<TA>,
       RunnableLike<RunnableLike<TB>>
     >,
-    Runnable_exhaust(),
+    Runnable_switchAll(),
   );
 
 export default Runnable_switchMap;
--- src/__tests__/Observable.test.ts
+++ src/__tests__/Observable.test.ts
@@ -1,16 +1,27 @@
 import * as Observable from "../Observable.js";
 import * as ReadonlyArray from "../ReadonlyArray.js";
+import * as Runnable from "../Runnable.js";
 import * as Scheduler from "../Scheduler.js";
 import {
   describe,
   expectArrayEquals,
   expectEquals,
   expectIsNone,
+  expectToThrow,
+  expectToThrowError,
   test,
   testAsync,
   testModule,
 } from "../__internal__/testing.js";
-import { Optional, bindMethod, pipe } from "../functions.js";
+import {
+  Optional,
+  arrayEquality,
+  bindMethod,
+  newInstance,
+  pipe,
+  pipeLazy,
+  returns,
+} from "../functions.js";
 import {
   DisposableLike_dispose,
   PublisherLike_observerCount,
@@ -62,6 +73,34 @@ testModule(
     }),
   ),
   describe(
+    "decodeWithCharset",
+    test("decoding ascii", () => {
+      const str = "abcdefghijklmnsopqrstuvwxyz";
+
+      pipe(
+        [str],
+        Observable.fromReadonlyArray(),
+        Observable.encodeUtf8(),
+        Observable.decodeWithCharset(),
+        Runnable.toReadonlyArray(),
+        x => x.join(),
+        expectEquals(str),
+      );
+    }),
+    test("decoding multi-byte code points", () => {
+      const str = String.fromCodePoint(8364);
+      pipe(
+        [str],
+        Observable.fromReadonlyArray(),
+        Observable.encodeUtf8(),
+        Observable.decodeWithCharset(),
+        Runnable.toReadonlyArray(),
+        x => x.join(),
+        expectEquals(str),
+      );
+    }),
+  ),
+  describe(
     "firstAsync",
     testAsync("empty source", async () => {
       const result = await pipe(
@@ -98,6 +137,149 @@ testModule(
       );
       pipe(result, expectEquals<Optional<number>>(3));
     }),
+  ),
+
+  describe(
+    "throwIfEmpty",
+    test("when source is empty", () => {
+      const error = new Error();
+      pipe(
+        pipeLazy(
+          [],
+          Observable.fromReadonlyArray(),
+          Observable.throwIfEmpty(() => error),
+          Runnable.toReadonlyArray(),
+        ),
+        expectToThrowError(error),
+      );
+    }),
+
+    test("when factory throw", () => {
+      const error = new Error();
+      pipe(
+        pipeLazy(
+          [],
+          Runnable.fromReadonlyArray(),
+          Observable.throwIfEmpty(() => {
+            throw error;
+          }),
+          Runnable.toReadonlyArray(),
+        ),
+        expectToThrowError(error),
+      );
+    }),
+
+    test(
+      "when source is not empty",
+      pipeLazy(
+        [1],
+        Observable.fromReadonlyArray(),
+        Observable.throwIfEmpty(() => undefined),
+        Runnable.toReadonlyArray<number>(),
+        expectArrayEquals([1]),
+      ),
+    ),
+  ),
+  describe(
+    "withLatestFrom",
+    test(
+      "when source and latest are interlaced",
+      pipeLazy(
+        [0, 1, 2, 3],
+        Observable.fromReadonlyArray({ delay: 1 }),
+        Observable.withLatestFrom(
+          pipe([0, 1, 2, 3], Observable.fromReadonlyArray({ delay: 2 })),
+          (a: number, b: number): [number, number] => [a, b],
+        ),
+        Runnable.toReadonlyArray(),
+        expectArrayEquals(
+          [
+            [0, 0],
+            [1, 0],
+            [2, 1],
+            [3, 1],
+          ],
+          arrayEquality(),
+        ),
+      ),
+    ),
+    test(
+      "when latest produces no values",
+      pipeLazy(
+        [0],
+        Observable.fromReadonlyArray({ delay: 1 }),
+        Observable.withLatestFrom(
+          Runnable.empty<number>(),
+          (a: number, b: number) => a + b,
+        ),
+        Runnable.toReadonlyArray(),
+        expectArrayEquals([] as number[]),
+      ),
+    ),
+    test("when latest throws", () => {
+      const error = newInstance(Error);
+
+      pipe(
+        pipeLazy(
+          [0],
+          Observable.fromReadonlyArray({ delay: 1 }),
+          Observable.withLatestFrom(
+            Observable.throws<number>({ raise: returns(error) }),
+            (a: number, b) => a + b,
+          ),
+          Runnable.toReadonlyArray(),
+          expectArrayEquals([] as number[]),
+        ),
+        expectToThrowError(error),
+      );
+    }),
+  ),
+
+  describe(
+    "zip",
+    test(
+      "with synchronous and non-synchronous sources",
+      pipeLazy(
+        Runnable.zip(
+          pipe([1, 2], Observable.fromReadonlyArray({ delay: 1 })),
+          pipe([2, 3], Observable.fromReadonlyArray()),
+          pipe([3, 4, 5], Observable.fromReadonlyArray({ delay: 1 })),
+        ),
+        Runnable.toReadonlyArray(),
+        expectArrayEquals(
+          [[1, 2, 3] as readonly number[], [2, 3, 4]],
+          arrayEquality(),
+        ),
+      ),
+    ),
+    test(
+      "fast with slow",
+      pipeLazy(
+        Runnable.zip(
+          pipe([1, 2, 3], Observable.fromReadonlyArray({ delay: 1 })),
+          pipe([1, 2, 3], Observable.fromReadonlyArray({ delay: 5 })),
+        ),
+        Runnable.toReadonlyArray(),
+        expectArrayEquals(
+          [[1, 1] as readonly number[], [2, 2], [3, 3]],
+          arrayEquality(),
+        ),
+      ),
+    ),
+    test(
+      "when source throws",
+      pipeLazy(
+        pipeLazy(
+          Runnable.zip(
+            Observable.throws(),
+            pipe([1, 2, 3], Observable.fromReadonlyArray()),
+          ),
+          Runnable.map<readonly [unknown, number], number>(([, b]) => b),
+          Runnable.toReadonlyArray(),
+        ),
+        expectToThrow,
+      ),
+    ),
   ),
 );
 
