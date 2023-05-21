b140eca6dffeabf0b46e639eb6678e0489e272df
David Bordoley <bordoley@gmail.com>
2023-05-21 00:31:18 -0400

    add promise tests

--- mod/__tests__/Promise.test.js
+++ mod/__tests__/Promise.test.js
@@ -1,10 +1,19 @@
 /// <reference types="./Promise.test.d.ts" />
 
+import * as EventSource from "../EventSource.js";
 import * as Observable from "../Observable.js";
 import * as PromiseT from "../Promise.js";
-import { describe, expectEquals, expectPromiseToThrow, testAsync, testModule, } from "../__internal__/testing.js";
+import { describe, expectArrayEquals, expectEquals, expectPromiseToThrow, testAsync, testModule, } from "../__internal__/testing.js";
 import { newInstance, pipe } from "../functions.js";
-testModule("Promise", describe("toObservable", testAsync("when the promise resolves", async () => {
+testModule("Promise", describe("toEventSource", testAsync("when the promise resolves", async () => {
+    const promise = Promise.resolve(1);
+    const result = await pipe(promise, PromiseT.toEventSource(), EventSource.toReadonlyArrayAsync());
+    pipe(result, expectArrayEquals([1]));
+}), testAsync("when the promise reject", async () => {
+    const error = newInstance(Error);
+    const promise = Promise.reject(error);
+    await pipe(pipe(promise, PromiseT.toEventSource(), EventSource.toReadonlyArrayAsync()), expectPromiseToThrow);
+})), describe("toObservable", testAsync("when the promise resolves", async () => {
     const promise = Promise.resolve(1);
     const result = await pipe(promise, PromiseT.toObservable(), Observable.lastAsync());
     pipe(result, expectEquals(1));
@@ -12,5 +21,13 @@ testModule("Promise", describe("toObservable", testAsync("when the promise resol
     const error = newInstance(Error);
     const promise = Promise.reject(error);
     await pipe(pipe(promise, PromiseT.toObservable(), Observable.lastAsync()), expectPromiseToThrow);
+})), describe("toReadonlyArrayAsync", testAsync("when the promise resolves", async () => {
+    const promise = Promise.resolve(1);
+    const result = await pipe(promise, PromiseT.toReadonlyArrayAsync());
+    pipe(result, expectArrayEquals([1]));
+}), testAsync("when the promise reject", async () => {
+    const error = newInstance(Error);
+    const promise = Promise.reject(error);
+    await pipe(pipe(promise, PromiseT.toReadonlyArrayAsync()), expectPromiseToThrow);
 })));
 ((_) => { })(PromiseT);
--- src/__tests__/Promise.test.ts
+++ src/__tests__/Promise.test.ts
@@ -1,7 +1,9 @@
+import * as EventSource from "../EventSource.js";
 import * as Observable from "../Observable.js";
 import * as PromiseT from "../Promise.js";
 import {
   describe,
+  expectArrayEquals,
   expectEquals,
   expectPromiseToThrow,
   testAsync,
@@ -11,6 +13,32 @@ import { Optional, newInstance, pipe } from "../functions.js";
 
 testModule(
   "Promise",
+  describe(
+    "toEventSource",
+    testAsync("when the promise resolves", async () => {
+      const promise = Promise.resolve(1);
+
+      const result = await pipe(
+        promise,
+        PromiseT.toEventSource(),
+        EventSource.toReadonlyArrayAsync(),
+      );
+      pipe(result, expectArrayEquals<Optional<number>>([1]));
+    }),
+    testAsync("when the promise reject", async () => {
+      const error = newInstance(Error);
+      const promise = Promise.reject(error);
+
+      await pipe(
+        pipe(
+          promise,
+          PromiseT.toEventSource(),
+          EventSource.toReadonlyArrayAsync(),
+        ),
+        expectPromiseToThrow,
+      );
+    }),
+  ),
   describe(
     "toObservable",
     testAsync("when the promise resolves", async () => {
@@ -29,6 +57,24 @@ testModule(
 
       await pipe(
         pipe(promise, PromiseT.toObservable(), Observable.lastAsync()),
+        expectPromiseToThrow,
+      );
+    }),
+  ),
+  describe(
+    "toReadonlyArrayAsync",
+    testAsync("when the promise resolves", async () => {
+      const promise = Promise.resolve(1);
+
+      const result = await pipe(promise, PromiseT.toReadonlyArrayAsync());
+      pipe(result, expectArrayEquals<Optional<number>>([1]));
+    }),
+    testAsync("when the promise reject", async () => {
+      const error = newInstance(Error);
+      const promise = Promise.reject(error);
+
+      await pipe(
+        pipe(promise, PromiseT.toReadonlyArrayAsync()),
         expectPromiseToThrow,
       );
     }),
