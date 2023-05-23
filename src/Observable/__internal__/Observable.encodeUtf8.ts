46f2556a210609cd4f985a73abd0aef8a23eb539
David Bordoley <bordoley@gmail.com>
2023-05-22 23:11:39 -0400

    encode utf8 tests

--- src/Observable/__internal__/Observable.encodeUtf8.ts
+++ src/Observable/__internal__/Observable.encodeUtf8.ts
@@ -1,19 +1,38 @@
-import type * as Observable from "../../Observable.js";
+import Enumerable_create from "../../Enumerable/__internal__/Enumerable.create.js";
+import * as Observable from "../../Observable.js";
 import {
   Function1,
   bindMethod,
+  compose,
+  ignore,
   invoke,
   newInstance,
   pipe,
   returns,
 } from "../../functions.js";
-import { ObservableLike, ObservableLike_observe } from "../../types.js";
+import { EnumerableLike, EnumerableLike_enumerate, ObservableLike, ObservableLike_observe } from "../../types.js";
 import Observable_createWithConfig from "./Observable.createWithConfig.js";
+import Observable_isEnumerable from "./Observable.isEnumerable.js";
 import Observable_map from "./Observable.map.js";
 
 const Observable_encodeUtf8: Observable.Signature["encodeUtf8"] =
   /*@__PURE__*/ returns(observable =>
-    Observable_createWithConfig(observer => {
+    Observable_isEnumerable<string>(observable) 
+      ? Enumerable_create(
+        () => {
+          const textEncoder = newInstance(TextEncoder);
+
+          return pipe(
+            observable as EnumerableLike<string>,
+            Observable_map<string, Uint8Array>(
+              bindMethod(textEncoder, "encode"),
+            ),
+            invoke(EnumerableLike_enumerate)
+          );
+
+        }
+      )
+      : Observable_createWithConfig(observer => {
       const textEncoder = newInstance(TextEncoder);
 
       pipe(
