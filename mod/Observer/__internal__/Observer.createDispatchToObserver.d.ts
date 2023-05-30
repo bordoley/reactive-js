a28b63d5213722e81b2f96c7691bbc293f0fc1c8
David Bordoley <bordoley@gmail.com>
2023-05-29 23:27:28 -0400

    remove unused file

--- mod/Observer/__internal__/Observer.createDispatchToObserver.d.ts
+++
@@ -1,3 +0,0 @@
-import { DispatcherLike, ObserverLike } from "../../types.js";
-declare const Observer_createDispatchToObserver: <T>(observer: ObserverLike<T>, dispatcher: DispatcherLike<T>) => ObserverLike<T>;
-export default Observer_createDispatchToObserver;
