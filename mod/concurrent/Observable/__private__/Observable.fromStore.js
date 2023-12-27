df37be09b96f2d3d60bcaef438f2c6b9c2516376
David Bordoley <bordoley@gmail.com>
2023-12-27 13:18:05 -0500

    make fromStore less heavy

--- mod/concurrent/Observable/__private__/Observable.fromStore.js
+++ mod/concurrent/Observable/__private__/Observable.fromStore.js
@@ -1,9 +1,14 @@
 /// <reference types="./Observable.fromStore.d.ts" />
 
+import { DispatcherLike_complete } from "../../../concurrent.js";
 import { StoreLike_value } from "../../../events.js";
-import { pipe } from "../../../functions.js";
-import Observable_fromEventSource from "./Observable.fromEventSource.js";
-import Observable_fromFactory from "./Observable.fromFactory.js";
-import Observable_mergeWith from "./Observable.mergeWith.js";
-const Observable_fromStore = () => (store) => pipe(() => store[StoreLike_value], Observable_fromFactory(), Observable_mergeWith(pipe(store, Observable_fromEventSource())));
+import * as EventSource from "../../../events/EventSource.js";
+import { bindMethod, pipe } from "../../../functions.js";
+import { QueueableLike_enqueue } from "../../../utils.js";
+import * as Disposable from "../../../utils/Disposable.js";
+import Observable_createMulticast from "./Observable.createMulticast.js";
+const Observable_fromStore = () => (store) => Observable_createMulticast(observer => {
+    observer[QueueableLike_enqueue](store[StoreLike_value]);
+    pipe(store, EventSource.addEventHandler(bindMethod(observer, QueueableLike_enqueue)), Disposable.onComplete(bindMethod(observer, DispatcherLike_complete)), Disposable.addTo(observer));
+});
 export default Observable_fromStore;
--- src/concurrent/Observable/__private__/Observable.fromStore.ts
+++ src/concurrent/Observable/__private__/Observable.fromStore.ts
@@ -1,17 +1,26 @@
+import { DispatcherLike_complete } from "../../../concurrent.js";
 import { StoreLike, StoreLike_value } from "../../../events.js";
-import { pipe } from "../../../functions.js";
+import * as EventSource from "../../../events/EventSource.js";
+import { bindMethod, pipe } from "../../../functions.js";
+import { QueueableLike_enqueue } from "../../../utils.js";
+import * as Disposable from "../../../utils/Disposable.js";
 import type * as Observable from "../../Observable.js";
-import Observable_fromEventSource from "./Observable.fromEventSource.js";
-import Observable_fromFactory from "./Observable.fromFactory.js";
-import Observable_mergeWith from "./Observable.mergeWith.js";
+import Observable_createMulticast from "./Observable.createMulticast.js";
 
 const Observable_fromStore: Observable.Signature["fromStore"] =
   <T>() =>
   (store: StoreLike<T>) =>
-    pipe(
-      () => store[StoreLike_value],
-      Observable_fromFactory(),
-      Observable_mergeWith(pipe(store, Observable_fromEventSource())),
-    );
+    Observable_createMulticast<T>(observer => {
+      observer[QueueableLike_enqueue](store[StoreLike_value]);
+
+      pipe(
+        store,
+        EventSource.addEventHandler(
+          bindMethod(observer, QueueableLike_enqueue),
+        ),
+        Disposable.onComplete(bindMethod(observer, DispatcherLike_complete)),
+        Disposable.addTo(observer),
+      );
+    });
 
 export default Observable_fromStore;
