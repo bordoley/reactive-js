ac19b94b4d6eac340b51d284ae566046d14c841c
David Bordoley <bordoley@gmail.com>
2023-12-29 08:20:10 -0500

    tweak Enumerable.empty implementation

--- mod/collections/Enumerable/__private__/Enumerable.empty.js
+++ mod/collections/Enumerable/__private__/Enumerable.empty.js
@@ -1,6 +1,6 @@
 /// <reference types="./Enumerable.empty.d.ts" />
 
-import { pipeLazy } from "../../../functions.js";
+import { pipe, returns } from "../../../functions.js";
 import Enumerable_fromReadonlyArray from "./Enumerable.fromReadonlyArray.js";
-const Enumerable_empty = /*@__PURE__*/ (() => pipeLazy([], Enumerable_fromReadonlyArray()))();
+const Enumerable_empty = /*@__PURE__*/ (() => pipe([], Enumerable_fromReadonlyArray(), returns))();
 export default Enumerable_empty;
--- src/collections/Enumerable/__private__/Enumerable.empty.ts
+++ src/collections/Enumerable/__private__/Enumerable.empty.ts
@@ -1,11 +1,12 @@
-import { pipeLazy } from "../../../functions.js";
+import { pipe, returns } from "../../../functions.js";
 import type * as Enumerable from "../../Enumerable.js";
 import Enumerable_fromReadonlyArray from "./Enumerable.fromReadonlyArray.js";
 
 const Enumerable_empty: Enumerable.Signature["empty"] = /*@__PURE__*/ (() =>
-  pipeLazy(
+  pipe(
     [],
     Enumerable_fromReadonlyArray(),
+    returns,
   ))() as Enumerable.Signature["empty"];
 
 export default Enumerable_empty;
