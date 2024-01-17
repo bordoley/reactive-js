d58bf1bc70f8dc4a75479e7813cf1e14458b2690
David Bordoley <bordoley@gmail.com>
2024-01-17 00:11:41 -0500

    tweak CurrentTime.now

--- src/__internal__/CurrentTime.ts
+++ src/__internal__/CurrentTime.ts
@@ -6,14 +6,12 @@ export const now = /*@__PURE__*/ (() => {
   const { performance } = globalObject;
   const { hrtime } = globalObject[Global_process] ?? {};
 
-  if (isSome(performance)) {
-    return bindMethod(performance, Clock_now);
-  } else if (isFunction(hrtime)) {
-    return () => {
-      const hr = hrtime();
-      return hr[0] * 1000 + hr[1] / 1e6;
-    };
-  } else {
-    return bindMethod(Date, Clock_now);
-  }
+  return isSome(performance)
+    ? bindMethod(performance, Clock_now)
+    : isFunction(hrtime)
+    ? () => {
+        const hr = hrtime();
+        return hr[0] * 1000 + hr[1] / 1e6;
+      }
+    : bindMethod(Date, Clock_now);
 })();
