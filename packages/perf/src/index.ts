83dcf8e64b1e343539cc4130efbc767720c38c43
David Bordoley <bordoley@gmail.com>
2025-02-21 14:01:22 -0500

    change the perf tests to run over a larger input size

--- packages/perf/src/index.ts
+++ packages/perf/src/index.ts
@@ -7,10 +7,10 @@ import {
 } from "./perfTests.js";
 
 const doRun = async () => {
-  await run(map(10000));
-  await run(filterMapFusion(10000));
-  await run(filterMapReduce(10000));
-  await run(scanReduce(10000));
+  await run(map(1000000));
+  await run(filterMapFusion(1000000));
+  await run(filterMapReduce(1000000));
+  await run(scanReduce(1000000));
 };
 
 doRun();
