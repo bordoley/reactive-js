2718e997156fa3c66f5c3073d40bca37508419a3
David Bordoley <bordoley@gmail.com>
2025-02-23 15:07:01 -0500

    Tweak Disposable.create to be easier to debug

--- mod/utils/Disposable/__private__/Disposable.create.js
+++ mod/utils/Disposable/__private__/Disposable.create.js
@@ -1,7 +1,9 @@
 /// <reference types="./Disposable.create.d.ts" />
 
-import { createInstanceFactory } from "../../../__internal__/mixins.js";
+import { include, init, mixInstanceFactory, } from "../../../__internal__/mixins.js";
 import DisposableMixin from "../../__mixins__/DisposableMixin.js";
-const Disposable_create = 
-/*@__PURE__*/ createInstanceFactory(DisposableMixin);
+const Disposable_create = /*@__PURE__*/ (() => mixInstanceFactory(include(DisposableMixin), function CreateDisposable(instance) {
+    init(DisposableMixin, instance);
+    return instance;
+}))();
 export default Disposable_create;
--- src/utils/Disposable/__private__/Disposable.create.ts
+++ src/utils/Disposable/__private__/Disposable.create.ts
@@ -1,8 +1,19 @@
-import { createInstanceFactory } from "../../../__internal__/mixins.js";
+import {
+  include,
+  init,
+  mixInstanceFactory,
+} from "../../../__internal__/mixins.js";
+import { DisposableLike } from "../../../utils.js";
 import type * as Disposable from "../../Disposable.js";
 import DisposableMixin from "../../__mixins__/DisposableMixin.js";
 
-const Disposable_create: Disposable.Signature["create"] =
-  /*@__PURE__*/ createInstanceFactory(DisposableMixin);
+const Disposable_create: Disposable.Signature["create"] = /*@__PURE__*/ (() =>
+  mixInstanceFactory<DisposableLike, DisposableLike>(
+    include(DisposableMixin),
+    function CreateDisposable(instance: DisposableLike) {
+      init(DisposableMixin, instance);
+      return instance;
+    },
+  ))();
 
 export default Disposable_create;
