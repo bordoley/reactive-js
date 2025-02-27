9ffe053e1ce82a106c2e5601c3f42587c019769e
David Bordoley <bordoley@gmail.com>
2025-02-26 23:21:31 -0500

    cache abort signals created from disposables

--- mod/utils/DisposableContainer/__private__/DisposableContainer.toAbortSignal.js
+++ mod/utils/DisposableContainer/__private__/DisposableContainer.toAbortSignal.js
@@ -2,9 +2,16 @@
 
 import { bindMethod, newInstance, pipe } from "../../../functions.js";
 import DisposableContainer_onDisposed from "./DisposableContainer.onDisposed.js";
-const DisposableContainer_toAbortSignal = (disposable) => {
-    const abortController = newInstance(AbortController);
-    pipe(disposable, DisposableContainer_onDisposed(bindMethod(abortController, "abort")));
-    return abortController.signal;
-};
+const DisposableContainer_toAbortSignal = 
+/*@__PURE__*/ (() => {
+    const abortSignalCache = newInstance(WeakMap);
+    return (disposable) => abortSignalCache.get(disposable) ??
+        (() => {
+            const abortController = newInstance(AbortController);
+            pipe(disposable, DisposableContainer_onDisposed(bindMethod(abortController, "abort")));
+            const abortSignal = abortController.signal;
+            abortSignalCache.set(disposable, abortSignal);
+            return abortSignal;
+        })();
+})();
 export default DisposableContainer_toAbortSignal;
--- src/utils/DisposableContainer/__private__/DisposableContainer.toAbortSignal.ts
+++ src/utils/DisposableContainer/__private__/DisposableContainer.toAbortSignal.ts
@@ -4,13 +4,22 @@ import type * as DisposableContainer from "../../DisposableContainer.js";
 import DisposableContainer_onDisposed from "./DisposableContainer.onDisposed.js";
 
 const DisposableContainer_toAbortSignal: DisposableContainer.Signature["toAbortSignal"] =
-  (disposable: DisposableContainerLike): AbortSignal => {
-    const abortController: AbortController = newInstance(AbortController);
-    pipe(
-      disposable,
-      DisposableContainer_onDisposed(bindMethod(abortController, "abort")),
-    );
-    return abortController.signal;
-  };
+  /*@__PURE__*/ (() => {
+    const abortSignalCache: WeakMap<DisposableContainerLike, AbortSignal> =
+      newInstance<WeakMap<DisposableContainerLike, AbortSignal>>(WeakMap);
+
+    return (disposable: DisposableContainerLike): AbortSignal =>
+      abortSignalCache.get(disposable) ??
+      (() => {
+        const abortController: AbortController = newInstance(AbortController);
+        pipe(
+          disposable,
+          DisposableContainer_onDisposed(bindMethod(abortController, "abort")),
+        );
+        const abortSignal = abortController.signal;
+        abortSignalCache.set(disposable, abortSignal);
+        return abortSignal;
+      })();
+  })();
 
 export default DisposableContainer_toAbortSignal;
