46d18e700c357f719f133f18172355fe3ac0e483
David Bordoley <bordoley@gmail.com>
2025-03-10 08:31:19 -0400

    simplify EventSource.create

--- mod/computations/EventSource/__private__/EventSource.create.js
+++ mod/computations/EventSource/__private__/EventSource.create.js
@@ -1,32 +1,16 @@
 /// <reference types="./EventSource.create.d.ts" />
 
-import { include, init, mixInstanceFactory, props, } from "../../../__internal__/mixins.js";
-import { ComputationLike_isDeferred, ComputationLike_isSynchronous, EventSourceLike_addEventListener, } from "../../../computations.js";
-import { error, none } from "../../../functions.js";
-import DelegatingDisposableMixin from "../../../utils/__mixins__/DelegatingDisposableMixin.js";
-import { DisposableLike_dispose, } from "../../../utils.js";
+import { error } from "../../../functions.js";
+import { DisposableLike_dispose } from "../../../utils.js";
 import * as Publisher from "../../Publisher.js";
-const EventSource_create = /*@__PURE__*/ (() => {
-    const CreateEventSource_delegate = Symbol("CreateEventSource_delegate");
-    return mixInstanceFactory(include(DelegatingDisposableMixin), function CreateEventSource(setup, options) {
-        const delegate = (this[CreateEventSource_delegate] =
-            Publisher.create(options));
-        init(DelegatingDisposableMixin, this, delegate);
-        try {
-            setup(delegate);
-        }
-        catch (e) {
-            delegate[DisposableLike_dispose](error(e));
-        }
-        return this;
-    }, props({
-        [CreateEventSource_delegate]: none,
-    }), {
-        [ComputationLike_isDeferred]: false,
-        [ComputationLike_isSynchronous]: false,
-        [EventSourceLike_addEventListener](listener) {
-            this[CreateEventSource_delegate][EventSourceLike_addEventListener](listener);
-        },
-    });
-})();
+const EventSource_create = (setup, options) => {
+    const delegate = Publisher.create(options);
+    try {
+        setup(delegate);
+    }
+    catch (e) {
+        delegate[DisposableLike_dispose](error(e));
+    }
+    return delegate;
+};
 export default EventSource_create;
--- src/computations/EventSource/__private__/EventSource.create.ts
+++ src/computations/EventSource/__private__/EventSource.create.ts
@@ -1,80 +1,23 @@
-import {
-  include,
-  init,
-  mixInstanceFactory,
-  props,
-} from "../../../__internal__/mixins.js";
-import {
-  ComputationLike_isDeferred,
-  ComputationLike_isSynchronous,
-  EventSourceLike,
-  EventSourceLike_addEventListener,
-  PublisherLike,
-} from "../../../computations.js";
-import { SideEffect1, error, none } from "../../../functions.js";
-import DelegatingDisposableMixin from "../../../utils/__mixins__/DelegatingDisposableMixin.js";
-import {
-  DisposableLike,
-  DisposableLike_dispose,
-  EventListenerLike,
-} from "../../../utils.js";
+import { SideEffect1, error } from "../../../functions.js";
+import { DisposableLike_dispose, EventListenerLike } from "../../../utils.js";
 import type * as EventSource from "../../EventSource.js";
 import * as Publisher from "../../Publisher.js";
 
-const EventSource_create: EventSource.Signature["create"] = /*@__PURE__*/ (<
-  T,
->() => {
-  const CreateEventSource_delegate = Symbol("CreateEventSource_delegate");
+const EventSource_create: EventSource.Signature["create"] = <T>(
+  setup: SideEffect1<EventListenerLike<T>>,
+  options?: {
+    readonly autoDispose?: boolean;
+  },
+) => {
+  const delegate = Publisher.create<T>(options);
 
-  type TProperties = {
-    [CreateEventSource_delegate]: PublisherLike<T>;
-  };
+  try {
+    setup(delegate);
+  } catch (e) {
+    delegate[DisposableLike_dispose](error(e));
+  }
 
-  return mixInstanceFactory(
-    include(DelegatingDisposableMixin),
-    function CreateEventSource(
-      this: Pick<
-        EventSourceLike<T>,
-        | typeof EventSourceLike_addEventListener
-        | typeof ComputationLike_isSynchronous
-        | typeof ComputationLike_isDeferred
-      > &
-        TProperties,
-      setup: SideEffect1<EventListenerLike<T>>,
-      options?: {
-        readonly autoDispose?: boolean;
-      },
-    ): EventSourceLike<T> & DisposableLike {
-      const delegate = (this[CreateEventSource_delegate] =
-        Publisher.create<T>(options));
-
-      init(DelegatingDisposableMixin, this, delegate);
-
-      try {
-        setup(delegate);
-      } catch (e) {
-        delegate[DisposableLike_dispose](error(e));
-      }
-
-      return this;
-    },
-    props<TProperties>({
-      [CreateEventSource_delegate]: none,
-    }),
-    {
-      [ComputationLike_isDeferred]: false as const,
-      [ComputationLike_isSynchronous]: false as const,
-
-      [EventSourceLike_addEventListener](
-        this: TProperties,
-        listener: EventListenerLike,
-      ) {
-        this[CreateEventSource_delegate][EventSourceLike_addEventListener](
-          listener,
-        );
-      },
-    },
-  );
-})();
+  return delegate;
+};
 
 export default EventSource_create;
