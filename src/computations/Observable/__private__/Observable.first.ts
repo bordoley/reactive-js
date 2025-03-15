a120a84ce7e8039e8d2a565a29f3c2cbc26c5963
Dave Bordoley <bordoley@gmail.com>
2025-03-15 13:05:07 -0400

    simplified Obs.first implementation

--- src/computations/Observable/__private__/Observable.first.ts
+++ src/computations/Observable/__private__/Observable.first.ts
@@ -1,27 +1,12 @@
 import { SynchronousObservableLike } from "../../../computations.js";
-import { Function1, Optional, none, pipe } from "../../../functions.js";
+import { Function1, Optional, compose } from "../../../functions.js";
 import type * as Observable from "../../Observable.js";
-import Observable_forEach from "./Observable.forEach.js";
-import Observable_run from "./Observable.run.js";
+import Observable_last from "./Observable.last.js";
 import Observable_takeFirst from "./Observable.takeFirst.js";
 
-const Observable_first: Observable.Signature["first"] =
-  <T>(options?: {
-    readonly maxMicroTaskTicks?: number;
-  }): Function1<SynchronousObservableLike<T>, Optional<T>> =>
-  observable => {
-    let result: Optional<T> = none;
-
-    pipe(
-      observable,
-      Observable_takeFirst(),
-      Observable_forEach((v: T) => {
-        result = v;
-      }),
-      Observable_run(options),
-    );
-
-    return result;
-  };
+const Observable_first: Observable.Signature["first"] = <T>(options?: {
+  readonly maxMicroTaskTicks?: number;
+}): Function1<SynchronousObservableLike<T>, Optional<T>> =>
+  compose(Observable_takeFirst(), Observable_last(options));
 
 export default Observable_first;
