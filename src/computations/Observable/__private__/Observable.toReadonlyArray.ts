ffe95cd1c40598117d536c1ed8ae6ac92e0a7aab
Dave Bordoley <bordoley@gmail.com>
2025-03-15 14:05:48 -0400

    toReadonlyArray using producer/consumer

--- src/computations/Observable/__private__/Observable.toReadonlyArray.ts
+++ src/computations/Observable/__private__/Observable.toReadonlyArray.ts
@@ -1,14 +1,46 @@
-import { SynchronousObservableLike } from "../../../computations.js";
-import { Function1, pipe } from "../../../functions.js";
+import {
+  ProducerLike_consume,
+  SynchronousObservableLike,
+} from "../../../computations.js";
+import {
+  Function1,
+  Optional,
+  invoke,
+  isSome,
+  pipe,
+  raise,
+} from "../../../functions.js";
+import * as Queue from "../../../utils/Queue.js";
+import * as VirtualTimeScheduler from "../../../utils/VirtualTimeScheduler.js";
+import {
+  DisposableLike_error,
+  VirtualTimeSchedulerLike_run,
+} from "../../../utils.js";
 import type * as Observable from "../../Observable.js";
-import Observable_buffer from "./Observable.buffer.js";
-import Observable_first from "./Observable.first.js";
+import Observable_toProducer from "./Observable.toProducer.js";
 
 const Observable_toReadonlyArray: Observable.Signature["toReadonlyArray"] =
   <T>(options?: {
     readonly maxMicroTaskTicks?: number;
   }): Function1<SynchronousObservableLike<T>, ReadonlyArray<T>> =>
-  observable =>
-    pipe(observable, Observable_buffer<T>(), Observable_first(options)) ?? [];
+  observable => {
+    using vts = VirtualTimeScheduler.create(options);
+    const queue = Queue.createCollector<T>({autoDispose: true})
+
+    pipe(
+      observable,
+      Observable_toProducer(vts),
+      invoke(ProducerLike_consume, queue),
+    );
+
+    vts[VirtualTimeSchedulerLike_run]();
+
+    const err = queue[DisposableLike_error];
+    if (isSome(err)) {
+      raise(err);
+    }
+
+    return queue.values;
+  };
 
 export default Observable_toReadonlyArray;
