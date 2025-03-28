602c7d9a4f7e5f6804594dc23da598d57e847f04
Dave Bordoley <bordoley@gmail.com>
2025-03-28 12:18:16 -0400

    fix withBackpressure bug

--- src/computations/Observable/__private__/Observable.withBackpressure.ts
+++ src/computations/Observable/__private__/Observable.withBackpressure.ts
@@ -1,5 +1,5 @@
-import { ObservableLike } from "../../../computations.js";
-import { identity } from "../../../functions.js";
+import { ObservableLike, SourceLike_subscribe } from "../../../computations.js";
+import { bindMethod, identity } from "../../../functions.js";
 import { BackpressureStrategy } from "../../../utils.js";
 import type * as Observable from "../../Observable.js";
 import * as DeferredSource from "../../__internal__/DeferredSource.js";
@@ -11,12 +11,19 @@ const Observable_withBackpressure: Observable.Signature["withBackpressure"] = (<
     capacity: number;
     backpressureStrategy: BackpressureStrategy;
   }) =>
-  (source: ObservableLike<T>): ObservableLike<T> =>
-    DeferredSource.createLifted(
+  (source: ObservableLike<T>): ObservableLike<T> => {
+    const lifted = DeferredSource.createLifted(
       source,
       identity,
       liftedSinkToObserverWithBackPressure(config),
       source,
-    )) as Observable.Signature["withBackpressure"];
+    );
+
+    // Ensures that upstream callers don't lift away the backpressure.
+    return DeferredSource.create(
+      bindMethod(lifted, SourceLike_subscribe),
+      source,
+    );
+  }) as Observable.Signature["withBackpressure"];
 
 export default Observable_withBackpressure;
