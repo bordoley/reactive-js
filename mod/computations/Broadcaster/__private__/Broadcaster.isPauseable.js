7b1a97f6947c754a1d2d9fda1395e51115bd1f7f
David Bordoley <bordoley@gmail.com>
2025-03-31 00:13:46 -0400

    delete unused code

--- mod/computations/Broadcaster/__private__/Broadcaster.isPauseable.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="./Broadcaster.isPauseable.d.ts" />
-
-import { isSome } from "../../../functions.js";
-import { PauseableLike_isPaused } from "../../../utils.js";
-const Broadcaster_isPauseable = (broadcaster) => isSome(broadcaster[PauseableLike_isPaused]);
-export default Broadcaster_isPauseable;
--- mod/utils/__mixins__/DelegatingObserverMixin.d.ts
+++
@@ -1,8 +0,0 @@
-import { Mixin1 } from "../../__internal__/mixins.js";
-import { DisposableLike, ObserverLike } from "../../utils.js";
-import { DelegatingConsumerLike } from "./DelegatingConsumerMixin.js";
-export interface DelegatingObserverLike<T, TOut = T, TDelegateObserver extends ObserverLike<TOut> = ObserverLike<TOut>> extends DelegatingConsumerLike<T, TOut, TDelegateObserver>, ObserverLike<T> {
-}
-type TReturn<T, TOut = T, TDelegateObserver extends ObserverLike<TOut> = ObserverLike<TOut>> = Omit<DelegatingObserverLike<T, TOut, TDelegateObserver>, keyof DisposableLike>;
-declare const DelegatingObserverMixin: <T, TOut = T, TDelegateObserver extends ObserverLike<TOut> = ObserverLike<TOut>>() => Mixin1<TReturn<T, TOut, TDelegateObserver>, TDelegateObserver>;
-export default DelegatingObserverMixin;
