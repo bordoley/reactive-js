61a799d87b4d8b43901049b1da225b8f91178a78
Dave Bordoley <bordoley@gmail.com>
2025-04-02 01:13:47 -0400

    Broadcaster_toProducer don't dispose the consumer

--- mod/computations/Broadcaster/__private__/Broadcaster.toProducer.js
+++ mod/computations/Broadcaster/__private__/Broadcaster.toProducer.js
@@ -1,10 +1,13 @@
 /// <reference types="./Broadcaster.toProducer.d.ts" />
 
-import { EventSourceLike_subscribe, } from "../../../computations.js";
-import { returns } from "../../../functions.js";
+import { bindMethod, pipe, returns } from "../../../functions.js";
+import * as Disposable from "../../../utils/Disposable.js";
+import * as DisposableContainer from "../../../utils/DisposableContainer.js";
+import { EventListenerLike_notify, SinkLike_complete, } from "../../../utils.js";
 import * as DeferredEventSource from "../../__internal__/DeferredEventSource.js";
+import Broadcaster_addEventHandler from "./Broadcaster.addEventHandler.js";
 const Broadcaster_toProducer = 
 /*@__PURE__*/ returns((src) => DeferredEventSource.create((consumer) => {
-    src[EventSourceLike_subscribe](consumer);
+    pipe(src, Broadcaster_addEventHandler(bindMethod(consumer, EventListenerLike_notify)), DisposableContainer.onComplete(bindMethod(consumer, SinkLike_complete)), Disposable.addTo(consumer));
 }));
 export default Broadcaster_toProducer;
--- src/computations/Broadcaster/__private__/Broadcaster.toProducer.ts
+++ src/computations/Broadcaster/__private__/Broadcaster.toProducer.ts
@@ -1,16 +1,27 @@
+import { BroadcasterLike } from "../../../computations.js";
+import { bindMethod, pipe, returns } from "../../../functions.js";
+import * as Disposable from "../../../utils/Disposable.js";
+import * as DisposableContainer from "../../../utils/DisposableContainer.js";
 import {
-  BroadcasterLike,
-  EventSourceLike_subscribe,
-} from "../../../computations.js";
-import { returns } from "../../../functions.js";
-import { ConsumerLike } from "../../../utils.js";
+  ConsumerLike,
+  EventListenerLike_notify,
+  SinkLike_complete,
+} from "../../../utils.js";
 import type * as Broadcaster from "../../Broadcaster.js";
 import * as DeferredEventSource from "../../__internal__/DeferredEventSource.js";
+import Broadcaster_addEventHandler from "./Broadcaster.addEventHandler.js";
 
 const Broadcaster_toProducer: Broadcaster.Signature["toProducer"] =
   /*@__PURE__*/ returns((src: BroadcasterLike) =>
     DeferredEventSource.create((consumer: ConsumerLike) => {
-      src[EventSourceLike_subscribe](consumer);
+      pipe(
+        src,
+        Broadcaster_addEventHandler(
+          bindMethod(consumer, EventListenerLike_notify),
+        ),
+        DisposableContainer.onComplete(bindMethod(consumer, SinkLike_complete)),
+        Disposable.addTo(consumer),
+      );
     }),
   ) as Broadcaster.Signature["toProducer"];
 
